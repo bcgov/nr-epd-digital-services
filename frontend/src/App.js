a7021d495ea06644dc27d273f1711ab9b9235e83
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2022-09-23 10:33:21 -0700

    Rudimentary Api Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import getHello from './utilities/api/hello';
+import {getAllUsers, addUser, deleteUser} from './utilities/api/users';
+
+
+
 
 function App() {
+  const [hello,setHello] = useState(null)
+  const [users,setUsers] = useState(null)
+  useEffect(() =>{
+    getHello().then(r => setHello(r));
+    getUsers();
+  },[])
+  
+  function addDefaultUser(){  
+    addUser({name:"Banana Bruce", email: "banana@rama.ca"})
+    .then(()=>{
+      getUsers();
+    })
+  }
+
+  function getUsers(){
+    getAllUsers().then(u => setUsers(u));
+  }
+
+  function deleteListener(id){
+    deleteUser(id).then( ()=>{
+      getUsers();
+    });
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 12
-        </a>
+        <h1>Basic HelloWorld App</h1>
       </header>
+      <p>{hello || "No Data"}</p>
+      <div>
+        {users?.length ? users.map((u) =>{
+           return(
+            <>
+                <h3>{u.name} <br/> {u.email}</h3>
+                <p>{u.id}</p>
+                <button onClick={ () => deleteListener(u.id)}>Delete User</button>
+                <hr/>
+            </>
+            
+           )})            
+       :
+        <p>No User</p>
+       }
+      </div>
+
+          
+        
+      
+      <hr/>
+      <button onClick={addDefaultUser}>Add A Default User?</button>
     </div>
   );
 }
