9eecf6a1b3c0786c8378086f9bcdda04d8e8631a
midhun-aot <midhun.murali@aot-technologies.com>
2022-10-03 16:57:53 -0700

    Added Multiple Packages
    Added multiple packages,  bootstrap , router , material ui etc.
    added a basic session state manager to persist state at client side

---
+++ frontend/src/app/features/applications/ApplicationSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  application: [],
+  status: "idle",
+  error: null,
+};
+
+
+
+const applicationSlice = createSlice({
+  name: "applications",
+  initialState,
+  reducers: {   
+  }
+});
+
+
+
+export default applicationSlice.reducer;
