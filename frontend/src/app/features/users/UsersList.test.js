3a6908975f6b1074022a0c12a6404a3533e82641
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2022-10-04 16:20:31 -0700

    Added router to tests

--- frontend/src/app/features/users/UsersList.test.js
+++ frontend/src/app/features/users/UsersList.test.js
@@ -2,13 +2,15 @@ import UsersList from "./UsersList";
 import {Provider} from 'react-redux'
 import  {store}  from "../../Store";
 import { render, screen } from "@testing-library/react";
-
+import {
+    BrowserRouter as Router
+  } from "react-router-dom";
 
 //import { renderWithProviders } from "../../../utilities/test/TestUtils";
 
 test.todo("Need to write wrapper which injects arbitrary test state and provider")
 
-const wrapper = <Provider store={store}><UsersList/></Provider>
+const wrapper = <Provider store={store}><Router><UsersList/></Router></Provider>
 
 
 
