5c8c43a5690d4a4f3609c25abc63d629e5936d43
midhun-aot <midhun.murali@aot-technologies.com>
2022-10-13 16:20:17 -0700

    basic graphql implementation on backend api
    basic graphql implementation on backend api

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
 import { Users } from "./entities/users.entity";
+import { UsersResolver } from './users.resolver';
+import { ApplicationsService } from "src/applications/applications.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersResolver],
+  exports:[UsersService]
 })
 export class UsersModule {}
