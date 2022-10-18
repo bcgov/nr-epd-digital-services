39692212bc24d663dc1dc4c5e3d612d3bfef4228
midhun-aot <midhun.murali@aot-technologies.com>
2022-10-18 14:52:26 -0700

    Auth with keyclock
    Added basic keyclock auth for rest api and graphql api

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,16 +1,19 @@
 import { Resolver,Query,Mutation , Args} from '@nestjs/graphql';
 import { argsToArgsConfig } from 'graphql/type/definition';
+import { Resource, RoleMatchingMode, Roles } from 'nest-keycloak-connect';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
 import { Users } from './entities/users.entity';
 import { UsersService } from './users.service';
 
 @Resolver(()=>Users)
+@Resource('backend')
 export class UsersResolver {
 
     constructor(private userService: UsersService)
     {}
-
+    
+    @Roles({roles:['adminbackend'],mode: RoleMatchingMode.ANY})
     @Query(()=>[Users],{name:"findUsers"})
     findAll()
     {
@@ -18,6 +21,7 @@ export class UsersResolver {
        return   this.userService.findAll().then(x=> {return x});
     }
 
+    @Roles({roles:['admincrud'],mode: RoleMatchingMode.ANY})
     @Mutation(()=>Users,{name:"createUser"})
     createUser(@Args('user') user:CreateUserDto )
     {
