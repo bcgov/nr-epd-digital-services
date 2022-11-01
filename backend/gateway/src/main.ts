b0f41a5f570629d1beafa808aa93cc9d6c755c1f
midhun-aot <midhun.murali@aot-technologies.com>
2022-11-01 10:12:16 -0700

    deleted unwanted folders

--- backend/gateway/src/main.ts
+++ backend/gateway/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3010);
+
+  //just for time being added a delayed start
+  setTimeout(async ()=>{await app.listen(3010)},10000)  ;
 }
 bootstrap();
--- users/src/users/dto/create-user.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
-
-@InputType()
-export class CreateUserInput {
-  @Field()
-  name:string;
-}
