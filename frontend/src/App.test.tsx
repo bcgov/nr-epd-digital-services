3107387d36fa8c9c7981cd72390a8347dc63f2f5
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2022-11-15 13:01:47 -0800

    Frontend unit test fix for auth testing

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,24 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { Provider } from 'react-redux';
+import {useAuth} from "react-oidc-context";
 import {store} from './app/Store'
 
+jest.mock('react-oidc-context', () => ({
+  useAuth: jest.fn(),
+}))
+
+
 it('renders user list link', () => {
-  render(    
-    <Provider store={store}>
-      <App />
-    </Provider>  );
-  const userlist = screen.getByText(/Users/i);
+  (useAuth as jest.Mock).mockReturnValue(
+    {isAuthenticated: true}
+  )
+  render(
+    //<AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    //</AuthProvider>
+      );
+  const userlist = screen.getByText(/List of Users/i);
   expect(userlist).toBeInTheDocument();
 
 });
 
 test('renders add new user form link', () =>{
+  (useAuth as jest.Mock).mockReturnValue(
+    {isAuthenticated: true}
+  )
   render(    
-    <Provider store={store}>
-      <App />
-    </Provider>  );
-  const userform = screen.getByText(/Add new User/i)
+    //<AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    //</AuthProvider>
+    );
+  const userform = screen.getByText(/Add New User/i)
   expect(userform).toBeInTheDocument()
 })
 
