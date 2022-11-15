4b6874b133244ad4f7d9bf03a3791abc85c83913
midhun-aot <midhun.murali@aot-technologies.com>
2022-11-15 09:48:37 -0800

    deleting the not required files

--- backend/gateway/src/reports/dto/update-report.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateReportInput } from './create-report.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateReportInput extends PartialType(CreateReportInput) {
-  @Field(() => Int)
-  id: number;
-}
