a0da6141d0bf18a497b6ec9c373ce6330b4e4fbf
sakthi-aot <81777282+sakthi-aot@users.noreply.github.com>
2022-11-22 16:17:59 -0700

    update

--- backend/gateway/src/app.module.ts
+++ backend/gateway/src/app.module.ts
@@ -60,8 +60,8 @@ GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
     buildService: ({ url }) => new RequestHandler({ url }),
     supergraphSdl: new IntrospectAndCompose({
       subgraphs: [
-        { name: 'users', url:  process.env.USERS_MICROSERVICE_ENDPOINT? process.env.USERS_MICROSERVICE_ENDPOINT : 'http://users:3005/graphql' },
-        { name: 'applications', url: process.env.APPLICATION_MICROSERVICE_ENDPOINT? process.env.APPLICATION_MICROSERVICE_ENDPOINT : 'http://applications:3006/graphql' },
+        { name: 'users', url:  process.env.USERS_MICROSERVICE_ENDPOINT? process.env.USERS_MICROSERVICE_ENDPOINT : 'http://epd-backend-users:3005/graphql' },
+        { name: 'applications', url: process.env.APPLICATION_MICROSERVICE_ENDPOINT? process.env.APPLICATION_MICROSERVICE_ENDPOINT : 'http://epd-backend-applications:3006/graphql' },
       ],
     }),
   },})],
