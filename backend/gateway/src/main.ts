bc66ce6d58e3961f81ab2098e984b62e34062bb9
midhun-aot <midhun.murali@aot-technologies.com>
2022-11-21 17:18:59 -0800

    adding wait-on package to graphql gateway

--- backend/gateway/src/main.ts
+++ backend/gateway/src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as waitOn from 'wait-on';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  //just for time being added a delayed start
-  setTimeout(async ()=>{await app.listen(3010)},60000)  ;
+  waitOn({
+    resources: ['http://users:3005'],
+    interval: 100,
+    timeout: 120000,
+    validateStatus: function (status) {
+      console.log('status', status);
+      return status == 401;
+    },
+  })
+    .then(async () => {
+      console.log('users service up');
+
+      const app = await NestFactory.create(AppModule);
+      await app.listen(3010);
+      //just for time being added a delayed start
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+
 bootstrap();
