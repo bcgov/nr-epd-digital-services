728d736f20684cd753cbc6d286579554663f72c4
RyanBirtch-aot <104386035+RyanBirtch-aot@users.noreply.github.com>
2022-11-24 14:36:25 -0800

    Cleanup

--- frontend/src/app/features/landing/Landing.tsx
+++ frontend/src/app/features/landing/Landing.tsx
@@ -5,9 +5,6 @@ import { FormFactory } from "../../helpers/factory/FormFactory"
 import { ResponsiveNavigationSidebar } from "../../components/navigation/ResponsiveNavigationSidebar"
 import { useAuth } from "react-oidc-context"
 import { LoginPanel } from "./LoginPanel"
-import './Landing.css'
-
-
 
 const Landing = () =>{
     var forms = FormFactory(jsonforms.forms)
@@ -16,39 +13,6 @@ const Landing = () =>{
     return(
         <Container fluid className="landing-container" id="landing-container">
             <LoginPanel/>
-            {/* <Row  className="login-panel py-5 mb-3  " id="login-panel">
-                {LoginSpacer}
-                <Col id="idir-login-panel" className="text-center col-xs-12 mx-0 mb-2 border border-dark">
-                    <Dropdown id="login-selector-dropdown" >
-                        <Dropdown.Toggle id="login-selector-dropdown-toggle">
-                            Log In to my LRS Account
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu id="login-selector-dropdown-menu">
-                            <Dropdown.Header>Select Login Method</Dropdown.Header>
-                            <Dropdown.Item as="button" id="login-button-bceid" className="dropdown-item"
-                                    onClick={() => void auth.signinRedirect(
-                                            {extraQueryParams:{'kc_idp_hint':'bceid'}})}>
-                                    BCEID
-                            </Dropdown.Item>
-                            <Dropdown.Divider/>
-                            <Dropdown.Item as="button" id="login-button-idir" onClick={() => void auth.signinRedirect(
-                                        {extraQueryParams:{'kc_idp_hint':'oidc'}})}>
-                                    IDIR
-                            </Dropdown.Item>
-
-                        </Dropdown.Menu>
-                        
-                    </Dropdown>
-                    
-                </Col>
-                <Col id="lrs-signup-panel" className="text-center border border-dark col-xs-12 mx-0">
-                    <Button id="lrs-signup-button" 
-                        onClick={() => void auth.signinRedirect(
-                                {extraQueryParams:{'kc_idp_hint':'bceid'}})}>
-                        Sign Up for an Account</Button>
-                </Col>
-                {LoginSpacer}
-            </Row> */}
             <Row id="responsive-lrs-nav">
                 <Col sm="12" md="3">
                     <ResponsiveNavigationSidebar/>
@@ -58,7 +22,6 @@ const Landing = () =>{
                     {LRSFormAccordion(forms)}
                 </Col>
             </Row>
-            
         </Container>
         )
 }
