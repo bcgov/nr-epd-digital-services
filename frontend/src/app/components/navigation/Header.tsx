f5641af8388a95c33f00060e2be25349036c5518
RyanBirtch-aot <104386035+RyanBirtch-aot@users.noreply.github.com>
2022-11-24 16:14:29 -0800

    cleanup 2

--- frontend/src/app/components/navigation/Header.tsx
+++ frontend/src/app/components/navigation/Header.tsx
@@ -27,17 +27,7 @@ const Header = () => {
         )
         :
         (
-          <React.Fragment>
-              {/* <button  className="btn btn-warning" onClick={() => void auth.signinRedirect()}>LogIn</button> */}
-
-              {/* <button  className="btn btn-success" onClick={() => void auth.signinRedirect({extraQueryParams:{
-                'kc_idp_hint':'oidc'
-              }})}>LogIn with IDIR</button>
-
-              <button  className="btn btn-info" onClick={() => void auth.signinRedirect({extraQueryParams:{
-                'kc_idp_hint':'bceid'
-              }})}>LogIn with BCEID</button> */}
-          </React.Fragment>
+          <></>
         )
       }
     
