7c07916874fe0cd32c11e89700a2499617ab8c24
RyanBirtch-aot <104386035+RyanBirtch-aot@users.noreply.github.com>
2022-11-24 17:27:24 -0800

    Fixed sonarcloud code smells 2

--- frontend/src/app/helpers/factory/FormFactory.ts
+++ frontend/src/app/helpers/factory/FormFactory.ts
@@ -2,7 +2,7 @@ import { LRSForm } from "../../components/landing/LRSFormAccordion";
 
 export const FormFactory = (raw:Array<{name:string, copy:string, url:string}>):Array<LRSForm> => {
     let forms = new Array<LRSForm>()
-    raw.map((f) =>{
+    raw.forEach((f) =>{
         forms.push( {...f} as LRSForm)
     })
     return forms
