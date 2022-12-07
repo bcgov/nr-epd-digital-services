f8606239b58bfbf74173b9f14ec5e3cc157ede76
RyanBirtch-aot <104386035+RyanBirtch-aot@users.noreply.github.com>
2022-12-06 17:59:36 -0800

    added config changes

--- frontend/src/app/helpers/endpoints.ts
+++ frontend/src/app/helpers/endpoints.ts
@@ -2,4 +2,4 @@ export const USERS = "/users";
 
 export const GRAPHQL= "/graphql"
 
-export const API:string = process.env.REACT_APP_BACKEND_API||'';
+export const API:string = process.env.REACT_APP_BACKEND_API|| ((window as any)._env_ && (window as any)._env_.REACT_APP_BACKEND_API) 
