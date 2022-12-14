8b4628f18816587b47407362da697b81942d4416
nikhila-aot <nikhila.pankaj@aot-technologies.com>
2022-12-13 16:10:23 -0800

    removed console logs

--- backend/gateway/src/RequestHandler.ts
+++ backend/gateway/src/RequestHandler.ts
@@ -1,13 +1,8 @@
 import { RemoteGraphQLDataSource } from '@apollo/gateway';
 
 export class RequestHandler extends RemoteGraphQLDataSource {
-  // async willSendRequest({request,context}) {
-  //     console.log('context', context.requestContext)
-  //    await request.http?.headers.set('somethingFromOriginalRequestOrSomethingCustom', "" )
-  // }
 
   willSendRequest({ request, context }) {
-    // console.log(context.userAuthToken)
     request.http?.headers.set('authorization', context.userAuthToken);
   }
 }
