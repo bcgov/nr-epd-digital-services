172932f779cb93d7206c276afd6e2fbc05df60cf
sakthi-aot <81777282+sakthi-aot@users.noreply.github.com>
2023-01-03 16:32:34 -0700

    update

--- backend/users/initDB.sh
+++ backend/users/initDB.sh
@@ -1,15 +1,5 @@
 
-#!/bin/sh
 
-#Make sure you change line endings to LF
-
-# if [ ! "$POSTGRESQL_HOST" ];
-# then
-#    echo 'Sourcing from .env'
-#    . ./.env
-# else
-#     echo 'Environment variables set...'
-# fi
 
 # create database taftdb
 psql "user=$POSTGRES_ADMIN_USERNAME password=$POSTGRES_ADMIN_PASSWORD host=$POSTGRESQL_HOST port=$POSTGRESQL_PORT" -c "CREATE DATABASE $POSTGRES_DATABASE OWNER $POSTGRES_ADMIN_USERNAME;"
