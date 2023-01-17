e5f7b3f36061ca7eed6e28aa300a2c35344ea55d
nikhila-aot <38471389+nikhila-aot@users.noreply.github.com>
2023-01-16 18:15:56 -0800

    null exception fix (#250)

--- backend/legacy/src/app.service.ts
+++ backend/legacy/src/app.service.ts
@@ -19,7 +19,7 @@ export class AppService {
         connection.close();
       })
       .catch(function (err) {
-        connection.close();
+        connection?.close();
         throw new Error(err);
       });
     return 'Successfully connected to Oracle Database';
