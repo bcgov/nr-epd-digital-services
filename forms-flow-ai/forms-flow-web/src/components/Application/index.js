3b2d95b54d739f182de736f206eca371da83e9ee
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2023-01-18 11:46:24 -0800

    Added BCSans font and style changes

---
+++ forms-flow-ai/forms-flow-web/src/components/Application/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import ApplicationList from "./List";
+import ViewApplication from "./ViewApplication";
+import "./Application.scss";
+import "./EPDStyling.scss";
+import { setCurrentPage } from "../../actions/bpmActions";
+import { BASE_ROUTE } from "../../constants/constants";
+
+export default React.memo(() => {
+  const showApplications = useSelector((state) => state.user.showApplications);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setCurrentPage("application"));
+  }, [dispatch]);
+
+  return (
+    <div className="container" id="main">
+      <Switch>
+        {showApplications ? (
+          <>
+            <Route
+              exact
+              path={`${BASE_ROUTE}application`}
+              component={ApplicationList}
+            />
+            <Route path={`${BASE_ROUTE}application/:applicationId`}>
+              <ViewApplication />
+            </Route>
+            <Route
+              path={`${BASE_ROUTE}application/:applicationId/:notavailable`}
+            >
+              <Redirect exact to="/404" />
+            </Route>
+          </>
+        ) : null}
+      </Switch>
+    </div>
+  );
+});
