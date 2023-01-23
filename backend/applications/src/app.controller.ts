5c6164f31c49f7fcfe8c710dc82fc6962b805283
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2023-01-23 12:21:08 -0800

    Print form post request, no authentication

--- backend/applications/src/app.controller.ts
+++ backend/applications/src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Req } from '@nestjs/common/decorators';
 import { Unprotected } from 'nest-keycloak-connect';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 @Unprotected()
@@ -10,5 +12,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  printFormSubmission(@Req() request: Request) {
+    console.log('Request from forms api');
+    console.log(request.headers);
   }
 }
--- backend/applications/src/app.service.ts
+++ backend/applications/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  confirmFormSubmission(formRequest: Request): any {
+    console.log('testing form submit');
+    return formRequest;
   }
 }
