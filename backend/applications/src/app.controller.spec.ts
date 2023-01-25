f8f122ade707897c35fc8c75aadb5400b39b67db
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2023-01-25 14:46:47 -0800

    Added Testing for echo endpoint

--- backend/applications/src/app.controller.spec.ts
+++ backend/applications/src/app.controller.spec.ts
@@ -1,6 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import { AuthGuard } from 'nest-keycloak-connect';
+
+const testPostBody = 'This is some data';
+const testUrl = 'http://localhost:3000';
+const testPostRequest: Partial<Request> = { body: testPostBody, url: testUrl };
+
+const MockAuthGuard = { canActivate: () => true };
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,14 +17,22 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
-    }).compile();
+    })
+      .overrideProvider(AuthGuard)
+      .useClass(MockAuthGuard)
+      .compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
+  describe('root healthtest', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+    it('should echo back request body', () => {
+      expect(
+        appController.printFormSubmission(testPostRequest as Request),
+      ).toEqual(testPostBody);
     });
   });
 });
