6fd421a4fecedf8d72b949dcdb3f7fe0232da7d5
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2023-03-07 10:36:55 -0800

    Fixed missed console.log

--- frontend/src/app/features/users/AddUserForm.test.tsx
+++ frontend/src/app/features/users/AddUserForm.test.tsx
@@ -2,15 +2,12 @@
 import { fireEvent, render, screen} from "@testing-library/react";
 import AddUserForm from './AddUserForm';
 import TestWrapper from '../../../utilities/test/TestUtils';
-import { useDispatch } from "react-redux";
-import { fetchUsers } from "./UsersSlice";
-import { store } from "../../Store";
+
 const wrapper = <TestWrapper><AddUserForm></AddUserForm></TestWrapper>
 
 const setup = () => {
     const utils = render(wrapper)
     const userNameInput = screen.getByLabelText("User Name:")
-    // const emailInput = utils.getByLabelText('Email:')
     const saveButton = screen.getByText("Save User")
     return {userNameInput, saveButton,utils}
 }
@@ -19,10 +16,7 @@ const setup = () => {
 test('Form Submits Correctly', () =>{
     const testUsername = new Date().getTime()
     const { userNameInput, saveButton,utils} = setup();
-    // fireEvent.change(emailInput, {target: {value: "testemail@test.com"}})
     fireEvent.change(userNameInput, {target: {value: testUsername}})
     fireEvent.click(saveButton)
-    // expect(emailInput).toBeEmpty()
     expect(userNameInput).toBeEmptyDOMElement()
-    console.log(store)
 })
