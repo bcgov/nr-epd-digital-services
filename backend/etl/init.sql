8ea515e796218d594bf88db076b7a8a7db767f26
jaise-aot <jaise.thomas@aot-technologies.com>
2023-04-13 00:17:26 -0700

    handle multiple tables, custom java transformer

--- backend/etl/init.sql
+++ backend/etl/init.sql
@@ -9,8 +9,25 @@ CREATE TABLE public.epd_user (
     primary key(user_id)
 );
 
+CREATE TABLE public.address (
+    addr_id int,
+    building_number int,
+    street_name varchar(50),
+    street_type varchar(20),
+    direction varchar(20),
+    city varchar(20),
+    province varchar(20),
+    postal_code varchar(20),
+    datetime_created timestamp,
+    datetime_updated timestamp,
+    primary key(addr_id)
+);
+
 ALTER TABLE public.epd_user replica identity FULL;
+ALTER TABLE public.address replica identity FULL;
 
 insert into public.epd_user values (1, 'Scott', 'Tiger', 'scott.tiger@xyz.com', 1, now(), now());
 insert into public.epd_user values (2, 'Micheal', 'Angello', 'micheal.angello@xyz.com', 1, now(), now());
 insert into public.epd_user values (3, 'Angel', 'DiMaria', 'angel.maria@xyz.com', 1, now(), now());
+
+insert into public.address values (1, '2255', 'Pandora', 'Ave', 'S', 'Victoria','B.C','V9B 2X4',now(), now());
