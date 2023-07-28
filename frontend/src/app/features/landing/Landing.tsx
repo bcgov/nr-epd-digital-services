54ec99ad2b7b06ce81d44b387f176611abb60402
Adam Coard <adam.coard@aot-technologies.com>
2023-07-28 08:31:51 -0700

    First pass of Haley's frontend changes

--- frontend/src/app/features/landing/Landing.tsx
+++ frontend/src/app/features/landing/Landing.tsx
@@ -10,7 +10,7 @@ const Landing = () =>{
     return(
         <Container fluid className="landing-container mt-3" id="landing-container">
             <LoginPanel/>
-            <Row id="responsive-lrs-nav" className="">
+            {/* <Row id="responsive-lrs-nav" className="">
                 <Col className="col-12 col-md-3" >
                     <ResponsiveNavigationSidebar/>
                 </Col>
@@ -22,7 +22,17 @@ const Landing = () =>{
                     {LRSFormAccordion(forms)}
                     </Row>
                 </Col>
+            </Row> */}
+
+            <Row>
+                <Col className="col-12 col-sm-6 offset-sm-3">
+                    <p>Collection Notice for Environmental Protection Platform:</p>
+                    <p>Your personal information is collected under 26(c) of the Freedom of Information and Protection of Privacy Act for the purpose of accessing services of the Environmental Protection Division. If you have any questions about the collection of your information, please contact Site Remediation Services at <a href="mailto:site@gov.bc.ca">site@gov.bc.ca</a>.</p>
+                    <p>Collection Notice for Site Remediation Forms:</p>
+                    <p>Your personal information is collected under 26(c) of the Freedom of Information and Protection of Privacy Act for the purpose of receiving site remediation services. If you have any questions about the collection of your information, please contact Site Remediation Services at <a href="mailto:site@gov.bc.ca">site@gov.bc.ca</a></p>        
+                </Col>
             </Row>
+
         </Container>
         )
 }
