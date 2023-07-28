8b7a22a0a7d64b237fe9ad3353b5acf395cf1f2a
Adam Coard <adam.coard@aot-technologies.com>
2023-07-28 09:04:09 -0700

    Removed sign-up button

--- frontend/src/app/components/landing/LoginPanel.tsx
+++ frontend/src/app/components/landing/LoginPanel.tsx
@@ -7,13 +7,13 @@ import "./LoginPanel.css"
 export const LoginPanel = () =>{
     const loginDropdownTitle = "Log in to my LRS Account";
     return(
-        <Row  className="login-panel py-5 mb-3  " id="login-panel">
-            <Col id="idir-login-panel" className="mt-auto mb-2 col-xs-12 col-md-6 text-center text-sm-end">
+        <Row  className="login-panel py-5 mb-3 " id="login-panel">
+            <Col id="idir-login-panel" className="mt-auto mb-2 text-center">
                 {LoginDropdown(loginDropdownTitle)}
             </Col>
-            <Col id="lrs-signup-panel" className="mt-auto mb-2 col-xs-12  col-md-6 text-center text-sm-start">
+            {/* <Col id="lrs-signup-panel" className="mt-auto mb-2 col-xs-12  col-md-6 text-center text-sm-start">
                 <Button id="lrs-signup-button" href="/signup" className="login-btn-lh">
                     Sign Up</Button>
-            </Col>        
+            </Col>         */}
         </Row>       
     )}
