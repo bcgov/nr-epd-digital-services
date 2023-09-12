a6e6ae1852590f911d017e8689252a251686d0a3
Adam Coard <adam.coard@aot-technologies.com>
2023-09-12 12:48:03 -0700

    Creating search results page prior to fake data generation, will stub in next.

--- site-search/vite/site-search/src/components/Header.tsx
+++ site-search/vite/site-search/src/components/Header.tsx
@@ -8,18 +8,18 @@ export default function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="/">BCGov Logo</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
+                {/* <Nav.Link href="#home">Home</Nav.Link> */}
+                {/* <Nav.Link href="#link">Link</Nav.Link> */}
+                <NavDropdown title="Search" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/">Basic Search</NavDropdown.Item>
+                  <NavDropdown.Item href="/advanced-search">
+                    Advanced Search
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Item href="/map">Map Search</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">
                     Separated link
--- site-search/vite/site-search/src/pages/search.tsx
+++ site-search/vite/site-search/src/pages/search.tsx
@@ -1,14 +1,20 @@
 import Button from 'react-bootstrap/Button';
 import Header from '@/components/Header'
+import { useState } from 'react';
+
+import { Link } from "react-router-dom";
+
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 
 export default function Search() {
+    const [searchBySiteID, setSearchBySiteId] = useState(false)
+
+
     return (
         <>
             <Header />
             <main className='container'>
-
-
                 <h2 className='text-center py-5'>Site Search</h2>
                 <div className="row">
                     <div className="col-sm-10">
@@ -16,12 +22,63 @@ export default function Search() {
                     </div>
 
                     <div className="col-sm">
-                        <a href='/advanced-search'>
+                        <Link to='/advanced-search'>
                             <Button variant='secondary'>Advanced Search</Button>
-                        </a>
+                        </Link>
                     </div>
-
                 </div>
+
+                <div className='bg-light p-3 mt-5'>
+                    <div className="row search-result-controls">
+                        <div className="col">
+                            <ToggleButton
+                                className="mb-2"
+                                id="toggle-check"
+                                type="checkbox"
+                                variant="outline-primary"
+                                checked={searchBySiteID}
+                                value="1"
+                                onChange={(e) => setSearchBySiteId(e.currentTarget.checked)}
+                            >
+                                Site ID
+                            </ToggleButton>
+                        </div>
+                    </div>
+                    <div className="row search-results mt-5">
+
+                        <div className="result">
+                            <div className="result-metadata">
+                                <a>View Site Details (TODO)</a>
+                                <a>View on Map (TODO)</a>
+                                <span className="float-end fst-italic">Last Updated: todotodo</span>
+
+                                <table className="table bg-light">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Site ID</th>
+                                            <th scope="col">Address/City</th>
+                                            <th scope="col">Latitude</th>
+                                            <th scope="col">Longitude</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">18326</th>
+                                            <td>123 Main St, Vancouver</td>
+                                            <td>49d 16m 46.8s</td>
+                                            <td>123d 4m 29.9s</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
             </main>
         </>
     )
