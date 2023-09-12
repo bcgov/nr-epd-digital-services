4ff3f5c7592c3d08a3a8eae47a3219fca6bc15c9
Adam Coard <adam.coard@aot-technologies.com>
2023-09-12 11:50:28 -0700

    Header dropdown to navigate to stubbed out advanced search page

--- site-search/site-search/components/Header.tsx
+++ site-search/site-search/components/Header.tsx
@@ -3,6 +3,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+import Link from 'next/link';
+import { NavLink } from 'react-bootstrap';
+
+
 export default function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
@@ -11,18 +15,14 @@ export default function Header() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Search</Nav.Link>
-                <Nav.Link href="#link">Dropdown</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
+                {/* WORKING! Took a lot of iterating to get Nav.Link and <Link> to play nice. */}
+                {/* <Nav.Link as="span"><Link href="/">Search</Link></Nav.Link>  */}
+                {/* <Nav.Link href="#link">Dropdown</Nav.Link> */}
+                <NavDropdown title="Search" id="basic-nav-dropdown">
+                  {/* <NavDropdown.Item href="#action/3.1">Basic Search</NavDropdown.Item> */}
+                  <Nav.Link as="span"><Link href="/">Basic Search</Link></Nav.Link> 
+                  <Nav.Link as="span"><Link href="/advanced-search">Advanced Search</Link></Nav.Link> 
+                  <Nav.Link as="span"><Link href="/map">Map Search</Link></Nav.Link> 
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
