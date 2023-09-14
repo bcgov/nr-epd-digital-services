72ca44d884d97263cca9a631d17a752404e22a81
Adam Coard <adam.coard@aot-technologies.com>
2023-09-14 10:54:15 -0700

    Delete old nextjs attempt

--- site-search/site-search/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
