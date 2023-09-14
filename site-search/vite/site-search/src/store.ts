ff723ac1fa229b6dfb7c193ea53faec5cb6f5a2c
Adam Coard <adam.coard@aot-technologies.com>
2023-09-14 10:54:40 -0700

    Delete old nextjs attempt - mk2

--- site-search/vite/site-search/src/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import siteReducer from '@/features/simple-search/simple-search'
-
-export default configureStore({
-  reducer: {
-    site: siteReducer
-  }
-})
