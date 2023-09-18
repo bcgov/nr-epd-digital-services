6ce8afae5f81e0019f3ec9ee027736beff7824ff
Adam Coard <adam.coard@aot-technologies.com>
2023-09-18 11:20:24 -0700

    Map styling

--- frontend/site-search-frontend/src/pages/map.tsx
+++ frontend/site-search-frontend/src/pages/map.tsx
@@ -4,30 +4,53 @@ import Button from 'react-bootstrap/Button';
 import { MapContainer, WMSTileLayer, Marker, Popup, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import styles from './map.module.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+
 
 
 export default function MapPage() {
-    const [location, setLocation] = useState([ 48.46762, -123.25458  ]);
+    const [location, setLocation] = useState([48.46762, -123.25458]);
 
     return (
         <>
             <Header />
-            <main className=''>
+            <main>
+                <div className={styles.controls}>
+                    <div className="d-flex justify-content-between">
 
-                <div className={styles.controls + " row"}>
-                    <div className='col-8 d-flex'>
-                        <input type="text" className="form-control d-inline-block" />
+                        <div className='d-flex'>
+                            <input type="text" className={styles.mapSearch + " form-control"} placeholder="Search site records..." />
 
-                        <Button variant="secondary-outline">Vector</Button>
-                        <Button variant="secondary-outline">Pin</Button>
-                    </div>
-                    <div className="col">
-                        <Button>Dropdown here</Button>
-                        <Button>Map/List Toggle here</Button>
+                            <Button className='ms-4' variant="secondary">Vector</Button>
+                            <Button className='mx-3' variant="secondary">Pin</Button>
+                        </div>
+                        <div className="d-flex">
+                            <Dropdown className='mx-3'>
+                                <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                    Data Layers
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item>todo</Dropdown.Item>
+                                    <Dropdown.Item>todo</Dropdown.Item>
+                                    <Dropdown.Item>todo</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+
+                            {/* <Button>Map/List Toggle here</Button> */}
+                            <ButtonGroup>
+                                <ToggleButton variant='secondary' value="false">
+                                    Map
+                                </ToggleButton>
+                                <ToggleButton variant='primary' value="1">
+                                    List
+                                </ToggleButton>
+                            </ButtonGroup>
+                        </div>
                     </div>
                 </div>
-
-                <p></p>
 
                 {/* <MapContainer center={location} zoom={13} style={{height: "calc(100vh-160px)", width: "100vw"}} > */}
                 {/* <MapContainer center={location} zoom={13} style={{height: "100vh", width: "100vw"}} > */}
