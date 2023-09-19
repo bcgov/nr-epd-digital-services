0d3a44b800ebcb9d851fe1dd774c8b1306de4926
Adam Coard <adam.coard@aot-technologies.com>
2023-09-19 09:03:29 -0700

    Remove logs

--- frontend/site-search-frontend/src/components/Highlighted.tsx
+++ frontend/site-search-frontend/src/components/Highlighted.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Highlighter = ({ children, highlight }) => {
     if (!highlight) return children;
-    console.log('CHILDREN', { children })
+    // console.log('CHILDREN', { children })
     const regexp = new RegExp(highlight, 'g');
     const matches = children.match(regexp);
     var parts = children.split(new RegExp(`${highlight.replace()}`, 'g'));
@@ -13,7 +13,7 @@ const Highlighter = ({ children, highlight }) => {
             let match = matches[i];
             // While the next part is an empty string, merge the corresponding match with the current
             // match into a single <span/> to avoid consequent spans with nothing between them.
-            console.log('PRE match', { match })
+            // console.log('PRE match', { match })
             while (parts[i + 1] === '') {
                 const newMatch = matches[++i];
                 // EPD - Custom fix here, making sure 'undefined' isn't appended after number records occasionally
