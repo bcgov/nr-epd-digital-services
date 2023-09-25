894f1e21f834f6c5bbe9b93d78c2f58a515dbeb9
Adam Coard <adam.coard@aot-technologies.com>
2023-09-25 11:25:24 -0700

    Fixed dummy data and properly instantiating site objects

--- frontend/site-search-frontend/src/features/simple-search/simple-search.ts
+++ frontend/site-search-frontend/src/features/simple-search/simple-search.ts
@@ -1,14 +1,17 @@
 import { createRandomSite } from '@/api/dummy-data'
 import { createSlice } from '@reduxjs/toolkit'
+import siteDummyData from '@/scripts/dummy-data.sites.json';
+import { Site } from '@/api/sites';
+
+// Instantiate dummy data, e.g. turn strings into Date objects
+const parsedDummyData = siteDummyData.map(siteData => new Site(siteData))
 
 
 export const siteSlice = createSlice({
     name: 'site',
     initialState: {
-        // value: [createRandomSite()]
-        // value: (new Array(100)).fill(createRandomSite)
-        value: Array.from({length: 250}, _ => createRandomSite())
-        // value: Array.from({length: 25000}, _ => createRandomSite())
+        // value: Array.from({length: 250}, _ => createRandomSite())
+        value: parsedDummyData
     },
     reducers: {
         add: ()=>console.log('site reducer add todo') // Maybe not even necessary? SITE list won't mutate much, unlike site selection
