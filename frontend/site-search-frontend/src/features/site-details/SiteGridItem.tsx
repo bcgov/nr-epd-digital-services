67237479bd5c8a8328681f31970a431c6f43c1fa
Adam Coard <adam.coard@aot-technologies.com>
2023-09-27 10:21:14 -0700

    Readonly styling of SiteGridItem

--- frontend/site-search-frontend/src/features/site-details/SiteGridItem.tsx
+++ frontend/site-search-frontend/src/features/site-details/SiteGridItem.tsx
@@ -3,12 +3,12 @@ import Form from 'react-bootstrap/Form';
 import styles from '@/pages/site-details.module.css'
 
 export default function SiteGridItem( {label, value, extraClasses = '', editMode}) {
-    // TODO: Get editMode from redux.
+    // TODO: Get editMode from redux?
 
     return (
         <div className={styles.metadataGridItem + " " + extraClasses} >
             <div className={styles.formLabel}>{label}</div>
-            <Form.Control plaintext={editMode} readOnly={!editMode} defaultValue={value} />
+            <Form.Control className={styles.formInput} readOnly={!editMode} defaultValue={value} />
         </div>
     )
 }
