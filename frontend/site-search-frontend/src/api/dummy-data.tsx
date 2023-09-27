78accb8cd1d76d17000c3997c4d25fbbadbe1184
Adam Coard <adam.coard@aot-technologies.com>
2023-09-27 12:18:05 -0700

    Dummy data generation for notations

--- frontend/site-search-frontend/src/api/dummy-data.tsx
+++ frontend/site-search-frontend/src/api/dummy-data.tsx
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import type { Site } from './sites'
+import { type Notation, type Site } from './sites'
 
 
 export function createRandomSite(): Site {
@@ -18,7 +18,12 @@ export function createRandomSite(): Site {
         victoriaFile: randomFileString(),
         regionalFile: 'N/A',
         parcelIDs: Array.from({length: 5}, _ => faker.number.int({min: 100000, max: 10000000})),
-        locationDescription: 'LAT/LONGS CONFIRMED USING ICIS MAY 16,2013'
+        locationDescription: 'LAT/LONGS CONFIRMED USING ICIS MAY 16,2013',
+
+        // Generate 1-5 notations
+        // notations: []
+        // notations: Array.from({length: faker.number.int({min: 2, max: 5}) }, () => { return randomNotation() })
+        notations: [randomNotation(), randomNotation()]
     }
 }
 
@@ -30,4 +35,29 @@ function randomRegion() {
 
 function randomFileString(){
     return `26250-20/${ faker.number.int({min: 700, max: 20000})}`
+}
+
+function randomNotation(): Notation {
+
+    function randomNotationParticipant() {
+        return {
+            name: faker.helpers.arrayElement(['SNC-LAVALIN ENVIRONMENT INC.', 'SHELL CANADA PRODUCTS']),
+            role: faker.helpers.arrayElement(['SUBMITTED BY', 'REQUESTED BY']),
+            siteRegistry: faker.datatype.boolean()
+        }
+    }
+
+    return ({
+        createdAt: faker.date.past({years: 10}),
+        completed: faker.date.past({years: 10}),
+        initiated: faker.date.past({years: 10}),
+        ministryContact: faker.person.lastName().toUpperCase() + " " + faker.person.firstName().toUpperCase(),
+        notationClass: "ENVIRONMENTAL MANAGEMENT ACT: GENERAL",
+        note: '',
+        notationType: 'CERTIFICATE OF COMPLIANCE REQUESTED',
+        requestedActions: [''],
+        // notationParticipants: [ Array.from({ length: faker.number.int({min: 1, max: 5}, () => { })  }) ]
+        notationParticipants: Array.from({length: faker.number.int({min: 2, max: 5})}, () => {return randomNotationParticipant()}) 
+        // notationParticipants: Array.from({length: 5}, () => {return randomNotationParticipant()}) 
+    })
 }
