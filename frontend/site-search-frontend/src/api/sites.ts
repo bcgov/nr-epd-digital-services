6a9d8e1e703695397482e328be8b2a66d89a8623
Adam Coard <adam.coard@aot-technologies.com>
2023-09-27 12:18:05 -0700

    Dummy data generation for notations

--- frontend/site-search-frontend/src/api/sites.ts
+++ frontend/site-search-frontend/src/api/sites.ts
@@ -14,10 +14,50 @@ export class Site {
     parcelIDs: number[];
     locationDescription: string;
 
+    notations: Notation[]
+
     constructor(data: Site) {
         // Using "Object.assign()" to bypass having to assign all properties
         // https://stackoverflow.com/questions/69291358/shortcut-syntax-to-class-constructor-from-typescript-interface
         Object.assign(this, data);
+
+        // This "new Date" is necessary when loading JSON dummy data which stores date as a string
+        // This will likely have to change when integrating with API
         this.lastUpdated = new Date(this.lastUpdated);
     }
 }
+
+// interface OnSiteRegistry {
+//     visibleOnSiteRegistry: boolean
+// }
+
+
+export class Notation {
+    createdAt: Date;
+    // notationType: NOTATION_TYPES
+    notationType: 'CERTIFICATE OF COMPLIANCE ISSUED USING RISK BASED STANDARDS' | 'CERTIFICATE OF COMPLIANCE REQUESTED'
+    notationClass: "ENVIRONMENTAL MANAGEMENT ACT: GENERAL"
+    initiated: Date;
+    completed: Date;
+    ministryContact: string;
+    note: string;
+    requestedActions: string[]
+    notationParticipants: {name: string, role: string, siteRegistry: boolean}[]
+
+    constructor(data: Notation) {
+        // See Site object for notes on constructor behaviour
+        Object.assign(this, data);
+        this.createdAt = new Date(this.createdAt);
+        this.initiated = new Date(this.initiated);
+        this.completed = new Date(this.completed);
+    }
+}
+
+// export enum NOTATION_TYPES {
+//     CERT_COMPLIANCE_ISSUED = 'CERTIFICATE OF COMPLIANCE ISSUED USING RISK BASED STANDARDS',
+//     CERT_COMPLIANCE_REQUESTED = 'CERTIFICATE OF COMPLIANCE REQUESTED'
+// }
+
+// export enum NOTATION_CLASS {
+//     ENV_GENERAL = 'ENVIRONMENTAL MANAGEMENT ACT: GENERAL'
+// }
