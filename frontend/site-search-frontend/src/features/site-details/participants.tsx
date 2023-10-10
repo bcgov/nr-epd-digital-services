c7da9e71e0accb08a8b2c562efaf3318d8ee9e02
Adam Coard <adam.coard@aot-technologies.com>
2023-10-10 11:05:29 -0700

    Have adding and deleting working on participant view

--- frontend/site-search-frontend/src/features/site-details/participants.tsx
+++ frontend/site-search-frontend/src/features/site-details/participants.tsx
@@ -1,16 +1,46 @@
 import { Button } from "react-bootstrap";
 import SubSearch from "./sub-search/SubSearch";
 import { RootState } from "@/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SiteDetailsTable from "./table/SiteDetailsTable";
 import { useParams } from "react-router-dom";
-import { Site } from "@/api/sites";
+import { Site, SiteParticipant } from "@/api/sites";
+// import { updateSite } from "../simple-search/simple-search";
+import { updateSite } from '../simple-search/simple-search';
 
 export default function Participants() {
     const { siteID } = useParams();
     const siteIDNum = parseInt(siteID);
     const site: Site = useSelector((state: RootState) => state.site.value.find(searchedSite => searchedSite.siteID === siteIDNum));
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
+
+    function newParticipant(){
+        const newParticipant: SiteParticipant = {name: '', endDate: '', roles: [''], notes: '', startDate: '', siteRegistry: false};
+        const newSite: Site = {
+            ...site,
+            participants: [...site.participants, newParticipant] 
+          };
+
+        dispatch(updateSite(newSite))
+    }
+
+    function removeParticipant(checkedObj){
+        const checkedAsArray = [...Object.keys(checkedObj).map(key => checkedObj[key])];
+        // console.log('remove', {checkedObj, checkedAsArray})
+
+        const newSite: Site = {
+            ...site,
+            participants: [...site.participants.filter((participant, index) => {
+                // if checked, we want to return false to remove it from the array
+                if (checkedObj[index] === true ) {
+                    return false
+                }
+                return true;
+            })]
+          };
+
+        dispatch(updateSite(newSite))
+    }
 
     return (
         <div>
@@ -27,6 +57,8 @@ export default function Participants() {
                     {label: 'Notes', accessor: 'notes'},
                 ]}
                 data={site.participants}
+                onClickAdd={newParticipant}
+                onClickRemove={removeParticipant}
             
             />
 
