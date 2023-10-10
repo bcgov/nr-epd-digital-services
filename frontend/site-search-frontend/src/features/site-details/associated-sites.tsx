e3dfb4d5f690ba6ee1a298287f98d92e10e3e877
Adam Coard <adam.coard@aot-technologies.com>
2023-10-10 08:59:48 -0700

    Associated sites table working, but site hyperlinks not working yet

--- frontend/site-search-frontend/src/features/site-details/associated-sites.tsx
+++ frontend/site-search-frontend/src/features/site-details/associated-sites.tsx
@@ -1,9 +1,33 @@
+import { Site } from "@/api/sites";
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import SubSearch from "./sub-search/SubSearch";
+import SiteDetailsTable from "./table/SiteDetailsTable";
 
 export default function AssociatedSites() {
+    const { siteID } = useParams();
+    const siteIDNum = parseInt(siteID);
+    const site: Site = useSelector((state: RootState) => state.site.value.find(searchedSite => searchedSite.siteID === siteIDNum));
 
     return (
         <div>
             <h4>Associated Sites go here</h4>
+            <SubSearch label='Associated Sites'></SubSearch>
+
+            {/* IDEA: Put in 'renderer' or 'component' option for each header? Should set component to use for that row
+            This will let us patch in custom link component?  Same with Date components. */}
+
+            <SiteDetailsTable
+                label='Associated Sites'
+                headers={[
+                    {label: 'Site ID', accessor: 'siteID'},
+                    {label: 'Parcel ID', accessor: 'parcelID'},
+                    {label: 'Date Noted', accessor: 'dateNoted'},
+                    {label: 'Notes', accessor: 'notes'}
+                ]}
+                data={site.associatedSites}
+            />
         </div>
     )
 }
