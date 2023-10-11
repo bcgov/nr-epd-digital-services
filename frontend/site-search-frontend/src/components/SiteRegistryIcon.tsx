76b89906d4cce2c36b3684e31883bbcb4b6b9b0a
Adam Coard <adam.coard@aot-technologies.com>
2023-10-11 09:13:18 -0700

    Site Registry Icon is now clickable and works in table, both features work fine

--- frontend/site-search-frontend/src/components/SiteRegistryIcon.tsx
+++ frontend/site-search-frontend/src/components/SiteRegistryIcon.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Eye, EyeSlash } from 'react-bootstrap-icons';
 
@@ -16,17 +16,26 @@ export default function SiteRegistryIcon({ siteRegistry }: { siteRegistry: boole
 }
 
 export function SiteRegistryIconButton({ siteRegistry }: { siteRegistry: boolean }) {
+    /**
+     * We create a "dummy" state object here.  In final version, would want to replace this with proper state change.
+     */
     const [dummySRState, setDummySRState] = useState<boolean>(siteRegistry)
+    function onClick() {
+        setDummySRState(!dummySRState)
+    }
+
+    /**
+     * This is to make the 'Make Selected Visible to Public' change work
+     * Otherwise, the change to the SiteRegistry prop will not change the value.
+     */
+    useEffect(() => {
+        setDummySRState(siteRegistry);
+    }, [siteRegistry])
 
     return (
         <>
-            <Button variant='link' onClick={() => setDummySRState(!dummySRState)}>
-                {/* For some reason need to do this ternary in here, couldn't just include <SiteRegistryIcon /> or wouldn't update on state changes 
-                eg Make Selected Visible To Public */}
-                {siteRegistry ?
-                    <Eye size='20' className='text-body' data-sr={siteRegistry} />
-                    : <EyeSlash size='20' className='text-body' data-sr={siteRegistry} />
-                }
+            <Button variant='link' onClick={onClick}>
+                <SiteRegistryIcon siteRegistry={dummySRState} /> 
             </Button>
         </>
     )
