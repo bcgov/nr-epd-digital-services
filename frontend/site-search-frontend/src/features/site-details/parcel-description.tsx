ed4594b17e41eaf8f25951d5c11f0eeaa99c9921
Adam Coard <adam.coard@aot-technologies.com>
2023-10-12 09:10:16 -0700

    Add/Remove table working with state updates on parcel and land use

--- frontend/site-search-frontend/src/features/site-details/parcel-description.tsx
+++ frontend/site-search-frontend/src/features/site-details/parcel-description.tsx
@@ -1,14 +1,42 @@
-import { Site } from "@/api/sites";
+import { ParcelDescription, Site } from "@/api/sites";
 import { RootState } from "@/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import SubSearch from "./sub-search/SubSearch";
 import SiteDetailsTable from "./table/SiteDetailsTable";
+import { faker } from "@faker-js/faker";
+import { updateSite } from "../simple-search/simple-search";
 
 export default function ParcelDescription() {
     const { siteID } = useParams();
     const siteIDNum = parseInt(siteID);
     const site: Site = useSelector((state: RootState) => state.site.value.find(searchedSite => searchedSite.siteID === siteIDNum));
+
+    const dispatch = useDispatch();
+
+    function newParcelDescription(){
+        const newParcel: ParcelDescription = {uuid: faker.string.uuid(), crownLandFileNumber: '', crownLandUsePIN: '', dateNoted: '', landDescription: '', parcelID: '', siteRegistry: false };
+        const newSite: Site = {
+            ...site,
+            parcelDescriptions: [...site.parcelDescriptions, newParcel]
+          };
+
+        dispatch(updateSite(newSite))
+    }
+
+    function removeParcel(checkedObj){
+        const newSite: Site = {
+            ...site,
+            parcelDescriptions: [...site.parcelDescriptions.filter((_, index) => {
+                if (checkedObj[index] === true ) {
+                    return false
+                }
+                return true;
+            })]
+          };
+
+        dispatch(updateSite(newSite))
+    }
 
 
     return (
@@ -24,6 +52,8 @@ export default function ParcelDescription() {
                     {label: 'Crown Lands File Number', accessor: 'crownLandFileNumber'},
                     {label: 'Land Description', accessor: 'landDescription'}
                 ]}
+                onClickAdd={newParcelDescription}
+                onClickRemove={removeParcel}
                 data={site.parcelDescriptions}
                 />
         </div>
