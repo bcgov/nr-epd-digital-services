3be5a8fa1169fa111516d28f683d6664a250847c
Adam Coard <adam.coard@aot-technologies.com>
2023-10-12 09:10:16 -0700

    Add/Remove table working with state updates on parcel and land use

--- frontend/site-search-frontend/src/features/site-details/suspect-land-uses.tsx
+++ frontend/site-search-frontend/src/features/site-details/suspect-land-uses.tsx
@@ -1,14 +1,41 @@
-import { Site } from "@/api/sites";
+import { Site, SuspectLandUse } from "@/api/sites";
 import { RootState } from "@/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import SubSearch from "./sub-search/SubSearch";
 import SiteDetailsTable from "./table/SiteDetailsTable";
+import { updateSite } from "../simple-search/simple-search";
+import { faker } from "@faker-js/faker";
 
 export default function SuspectLandUses() {
     const { siteID } = useParams();
     const siteIDNum = parseInt(siteID);
     const site: Site = useSelector((state: RootState) => state.site.value.find(searchedSite => searchedSite.siteID === siteIDNum));
+    const dispatch = useDispatch();
+
+    function newSuspectLandUse(){
+        const newLandUse: SuspectLandUse = {uuid: faker.string.uuid(), landUse: '', notes: '', siteRegistry: true};
+        const newSite: Site = {
+            ...site,
+            suspectLandUses: [...site.suspectLandUses, newLandUse]
+          };
+
+        dispatch(updateSite(newSite))
+    }
+
+    function removeLandUse(checkedObj){
+        const newSite: Site = {
+            ...site,
+            suspectLandUses: [...site.suspectLandUses.filter((_, index) => {
+                if (checkedObj[index] === true ) {
+                    return false
+                }
+                return true;
+            })]
+          };
+
+        dispatch(updateSite(newSite))
+    }
 
 
     return (
@@ -21,6 +48,8 @@ export default function SuspectLandUses() {
                     {label: 'Land Use', accessor: 'landUse'},
                     {label: 'Notes', accessor: 'notes'}
                 ]}
+                onClickAdd={newSuspectLandUse}
+                onClickRemove={removeLandUse}
                 data={site.suspectLandUses}
             />
 
