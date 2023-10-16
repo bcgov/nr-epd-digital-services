4ba7884bde3e9fd0a355ee9862de471ed2a6682e
Adam Coard <adam.coard@aot-technologies.com>
2023-10-16 09:06:45 -0700

    Associated Site creation

--- frontend/site-search-frontend/src/features/site-details/associated-sites.tsx
+++ frontend/site-search-frontend/src/features/site-details/associated-sites.tsx
@@ -1,14 +1,46 @@
-import { Site } from "@/api/sites";
+import { AssociatedSite, Site } from "@/api/sites";
 import { RootState } from "@/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import SubSearch from "./sub-search/SubSearch";
 import SiteDetailsTable from "./table/SiteDetailsTable";
+import { updateSite } from "../simple-search/simple-search";
+import { faker } from "@faker-js/faker";
 
 export default function AssociatedSites() {
     const { siteID } = useParams();
     const siteIDNum = parseInt(siteID);
     const site: Site = useSelector((state: RootState) => state.site.value.find(searchedSite => searchedSite.siteID === siteIDNum));
+    const dispatch = useDispatch();
+
+    function addAssociation(){
+        const newAssociation: AssociatedSite = {uuid: faker.string.uuid(), dateNoted: '', notes: '', parcelID: '', siteID: '', siteRegistry: false};
+        const newSite: Site = {
+            ...site,
+            associatedSites: [...site.associatedSites, newAssociation] 
+          };
+
+        dispatch(updateSite(newSite))
+    }
+
+    function removeAssociation(checkedObj){
+        // const checkedAsArray = [...Object.keys(checkedObj).map(key => checkedObj[key])];
+        // console.log('remove', {checkedObj, checkedAsArray})
+
+        const newSite: Site = {
+            ...site,
+            associatedSites: [...site.associatedSites.filter((_, index) => {
+                // if checked, we want to return false to remove it from the array
+                if (checkedObj[index] === true ) {
+                    // console.log('removing', participant)
+                    return false
+                }
+                return true;
+            })]
+          };
+
+        dispatch(updateSite(newSite))
+    }
 
     return (
         <div>
@@ -25,6 +57,8 @@ export default function AssociatedSites() {
                     {label: 'Date Noted', accessor: 'dateNoted'},
                     {label: 'Notes', accessor: 'notes'}
                 ]}
+                onClickAdd={addAssociation}
+                onClickRemove={removeAssociation}
                 data={site.associatedSites}
             />
         </div>
