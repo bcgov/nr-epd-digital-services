6dde68619cc7fd8699f2b3d3b1de27eac244745d
Adam Coard <adam.coard@aot-technologies.com>
2023-10-17 10:22:15 -0700

    Fix typing of user slice

--- frontend/site-search-frontend/src/features/simple-search/simple-search.ts
+++ frontend/site-search-frontend/src/features/simple-search/simple-search.ts
@@ -6,57 +6,11 @@ import { current } from '@reduxjs/toolkit'
 import formatDateToString from '@/helpers/formatDateToString';
 
 
-
-// Instantiate dummy data, e.g. turn strings into Date objects
-// Use `as any` to get around fact that 'lastUpdated' is a string, not a date.
-// Definitely causes redux issues though  - https://redux.js.org/faq/organizing-state#can-i-put-functions-promises-or-other-non-serializable-items-in-my-store-state
-// Should potentially only initialize date object in the view, not the store?
-// const parsedDummyData: Site[] = siteDummyData.map(siteData => new Site((siteData as any)))
-
-// const addNotationBySiteIDNewTemp = (state, action) => {
-//     const site: Site = state.value.find((todo) => todo.siteID === action.payload)
-//     // site.notations.push();
-//     // site.notations = [];
-//     site.notations = [...site.notations, {
-//         completed: new Date(),
-//         createdAt: new Date(),
-//         initiated: new Date(),
-//         ministryContact: '',
-//         notationClass: 'ENVIRONMENTAL MANAGEMENT ACT: GENERAL',
-//         notationParticipants: [],
-//         notationType: 'CERTIFICATE OF COMPLIANCE ISSUED USING RISK BASED STANDARDS',
-//         note: '',
-//         requestedActions: []
-//     }]
-// }
-
-// const addNotationBySiteIDNewTemp = (state, action) => {
-//     const site: Site = state.value.find((todo) => todo.siteID === action.payload)
-//     // site.notations.push();
-//     // site.notations = [];
-//     site.notations = [...site.notations, {
-//         completed: formatDateToString(new Date()),
-//         createdAt: formatDateToString(new Date()), 
-//         initiated: formatDateToString(new Date()), 
-//         ministryContact: '',
-//         notationClass: 'ENVIRONMENTAL MANAGEMENT ACT: GENERAL',
-//         notationParticipants: [],
-//         notationType: 'CERTIFICATE OF COMPLIANCE ISSUED USING RISK BASED STANDARDS',
-//         note: '',
-//         requestedActions: [],
-//         siteRegistry: false,
-//     }]
-
-//     // return state; // ???? can i do a ...spread patch in here?
-// }
-
-
 export const siteSlice = createSlice({
     name: 'site',
     initialState: {
         // value: Array.from({length: 250}, _ => createRandomSite())
-        // value: parsedDummyData
-        value: siteDummyData
+        value: siteDummyData as Site[]
     },
     reducers: {
         add: () => console.log('site reducer add todo'), // Maybe not even necessary? SITE list won't mutate much, unlike site selection
@@ -75,19 +29,6 @@ export const siteSlice = createSlice({
               site.notations = site.notations.filter((notation) => notation.uuid !== action.payload.notationUUID);
             }
           },
-
-        // deleteNotation: (state, action: PayloadAction<{ siteUUID: string, notationUUID: string }>) => {
-        //     const { siteUUID, notationUUID } = action.payload;
-      
-        //     // Find the site by UUID
-        //     const site = state.site.find(site => site.uuid === siteUUID);
-      
-        //     if (site) {
-        //       // Filter out the notation by UUID from the site's notations
-        //       site.notations = site.notations.filter(notation => notation.uuid !== notationUUID);
-        //     }
-        //   },
-
     }
 })
 
