d599aaf990d4c0fb3ac733eb43bcb610a6c0d7eb
Adam Coard <adam.coard@aot-technologies.com>
2023-10-17 11:15:39 -0700

    Subsearch responsive improvements

--- frontend/site-search-frontend/src/features/site-details/sub-search/SubSearch.tsx
+++ frontend/site-search-frontend/src/features/site-details/sub-search/SubSearch.tsx
@@ -14,7 +14,7 @@ export default function SubSearch({ label, showResultsDropdown = false }: SubSea
                 <div className={showResultsDropdown ? "col-8" : "col-10"}>
                     <input type="text" className={"form-control " + styles.searchBar} placeholder={`Search ${label}`} aria-label={`Search ${label}`} />
                 </div>
-                <div className='col-2'>
+                <div className='col-12 col-xl-2'>
                     <DropdownButton variant='outline-secondary' id="dropdown-notation-search-type" title="Sort results by: Date">
                         <Dropdown.Item eventKey='Date'>Date</Dropdown.Item>
                         <Dropdown.Item eventKey='???'>???</Dropdown.Item>
