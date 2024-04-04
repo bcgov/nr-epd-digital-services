0cc710755195bb095ef3cbbf7899df9f01a61476
nikhila-aot <nikhila.pankaj@aot-technologies.com>
2024-04-04 12:56:37 -0700

    added unit test for site service and resolver

--- backend/sites/src/app/dto/response/fetchSiteResponse.ts
+++ backend/sites/src/app/dto/response/fetchSiteResponse.ts
@@ -4,7 +4,7 @@ import { Sites } from '../../entities/sites.entity';
 import { BaseHttpResponse } from './baseHttpResponse';
 
 /**
- * Class for returing fetch region response from graphql services
+ * Class for returing fetch site response from graphql services
  */
 @ObjectType()
 export class FetchSiteResponse extends BaseHttpResponse {
