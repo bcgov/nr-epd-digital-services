2204eddf33a1a742596287c1d682cd183a189fda
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2024-04-10 13:31:48 -0700

    Change the end point to user api. Add new axios instance function.

--- frontend/src/app/helpers/endpoints.ts
+++ frontend/src/app/helpers/endpoints.ts
@@ -3,3 +3,5 @@ export const USERS = "/users";
 export const GRAPHQL= "/graphql"
 
 export const API:string = process.env.REACT_APP_BACKEND_API|| ((window as any)._env_ && (window as any)._env_.REACT_APP_BACKEND_API) 
+
+export const USERS_API: string =  process.env.REACT_APP_BACKEND_USERS_API|| ((window as any)._env_ && (window as any)._env_.REACT_APP_BACKEND_USERS_API) 
