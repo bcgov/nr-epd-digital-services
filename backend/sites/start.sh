ce9ce865688d510f71c848d9e65c9b78e0bc69e5
sakthi-aot <sakthi.saranya@aot-technologies.com>
2024-04-23 12:47:06 -0600

    site build issue

--- backend/sites/start.sh
+++ backend/sites/start.sh
@@ -7,8 +7,15 @@
 # Creates a "dist" folder with the production build
 #npm run build
 
-# Change ownership of .npm folder
-chown -R node:node /home/node/.npm
+#!/bin/sh
 
-# start the API 
+# Check if .npm directory exists
+if [ -d "/home/node/.npm" ]; then
+  # Change ownership of .npm folder
+  chown -R node:node /home/node/.npm
+else
+  echo "Directory '/home/node/.npm' not found. Skipping ownership change."
+fi
+
+# Start the API
 npm run start:prod
