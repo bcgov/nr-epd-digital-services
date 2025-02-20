17d8f919bdd495623f83e544d186ab3d37e308fd
Anton <anton.trublin+bcgov@gmail.com>
2025-02-20 13:54:31 -0800

    WIP: vite migration

--- cats-frontend/babel.config.js
+++ cats-frontend/babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
-    '@babel/preset-typescript',
-    '@babel/preset-env',
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    "@babel/preset-typescript",
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }],
   ],
+  env: {
+    test: {
+      plugins: ["babel-plugin-transform-import-meta"],
+    },
+  },
 };
