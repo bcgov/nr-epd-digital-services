d77a56811bceabe898cf58883dcbae459b9a75cf
nupurdixit13 <nupur.dixit@gov.bc.ca>
2025-03-04 12:06:12 -0800

    Fix Dto class

--- cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/hooks/Participants.generated.tsx
+++ cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/hooks/Participants.generated.tsx
@@ -7,31 +7,50 @@ export type GetAppParticipantsByAppIdQueryVariables = Types.Exact<{
   applicationId: Types.Scalars['Int']['input'];
 }>;
 
-
-export type GetAppParticipantsByAppIdQuery = { __typename?: 'Query', getAppParticipantsByAppId: { __typename?: 'AppParticipantsResponse', httpStatusCode?: number | null, success?: boolean | null, message?: string | null, timestamp?: string | null, data?: Array<{ __typename?: 'AppParticipantsDto', id: number, applicationId: number, isMainParticipant: boolean, name: string, fullName: string, description: string, effectiveStartDate: any, effectiveEndDate?: any | null, isMinistry: boolean }> | null } };
-
+export type GetAppParticipantsByAppIdQuery = {
+  __typename?: 'Query';
+  getAppParticipantsByAppId: {
+    __typename?: 'AppParticipantsResponse';
+    httpStatusCode?: number | null;
+    success?: boolean | null;
+    message?: string | null;
+    timestamp?: string | null;
+    data?: Array<{
+      __typename?: 'ViewAppParticipantsDto';
+      id: number;
+      applicationId: number;
+      isMainParticipant: boolean;
+      name: string;
+      fullName: string;
+      description: string;
+      effectiveStartDate: any;
+      effectiveEndDate?: any | null;
+      isMinistry: boolean;
+    }> | null;
+  };
+};
 
 export const GetAppParticipantsByAppIdDocument = gql`
-    query getAppParticipantsByAppId($applicationId: Int!) {
-  getAppParticipantsByAppId(applicationId: $applicationId) {
-    httpStatusCode
-    success
-    message
-    timestamp
-    data {
-      id
-      applicationId
-      isMainParticipant
-      name
-      fullName
-      description
-      effectiveStartDate
-      effectiveEndDate
-      isMinistry
+  query getAppParticipantsByAppId($applicationId: Int!) {
+    getAppParticipantsByAppId(applicationId: $applicationId) {
+      httpStatusCode
+      success
+      message
+      timestamp
+      data {
+        id
+        applicationId
+        isMainParticipant
+        name
+        fullName
+        description
+        effectiveStartDate
+        effectiveEndDate
+        isMinistry
+      }
     }
   }
-}
-    `;
+`;
 
 /**
  * __useGetAppParticipantsByAppIdQuery__
@@ -49,19 +68,61 @@ export const GetAppParticipantsByAppIdDocument = gql`
  *   },
  * });
  */
-export function useGetAppParticipantsByAppIdQuery(baseOptions: Apollo.QueryHookOptions<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables> & ({ variables: GetAppParticipantsByAppIdQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>(GetAppParticipantsByAppIdDocument, options);
-      }
-export function useGetAppParticipantsByAppIdLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>(GetAppParticipantsByAppIdDocument, options);
-        }
-export function useGetAppParticipantsByAppIdSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>) {
-          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
-          return Apollo.useSuspenseQuery<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>(GetAppParticipantsByAppIdDocument, options);
-        }
-export type GetAppParticipantsByAppIdQueryHookResult = ReturnType<typeof useGetAppParticipantsByAppIdQuery>;
-export type GetAppParticipantsByAppIdLazyQueryHookResult = ReturnType<typeof useGetAppParticipantsByAppIdLazyQuery>;
-export type GetAppParticipantsByAppIdSuspenseQueryHookResult = ReturnType<typeof useGetAppParticipantsByAppIdSuspenseQuery>;
-export type GetAppParticipantsByAppIdQueryResult = Apollo.QueryResult<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>;
+export function useGetAppParticipantsByAppIdQuery(
+  baseOptions: Apollo.QueryHookOptions<
+    GetAppParticipantsByAppIdQuery,
+    GetAppParticipantsByAppIdQueryVariables
+  > &
+    (
+      | { variables: GetAppParticipantsByAppIdQueryVariables; skip?: boolean }
+      | { skip: boolean }
+    ),
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<
+    GetAppParticipantsByAppIdQuery,
+    GetAppParticipantsByAppIdQueryVariables
+  >(GetAppParticipantsByAppIdDocument, options);
+}
+export function useGetAppParticipantsByAppIdLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    GetAppParticipantsByAppIdQuery,
+    GetAppParticipantsByAppIdQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<
+    GetAppParticipantsByAppIdQuery,
+    GetAppParticipantsByAppIdQueryVariables
+  >(GetAppParticipantsByAppIdDocument, options);
+}
+export function useGetAppParticipantsByAppIdSuspenseQuery(
+  baseOptions?:
+    | Apollo.SkipToken
+    | Apollo.SuspenseQueryHookOptions<
+        GetAppParticipantsByAppIdQuery,
+        GetAppParticipantsByAppIdQueryVariables
+      >,
+) {
+  const options =
+    baseOptions === Apollo.skipToken
+      ? baseOptions
+      : { ...defaultOptions, ...baseOptions };
+  return Apollo.useSuspenseQuery<
+    GetAppParticipantsByAppIdQuery,
+    GetAppParticipantsByAppIdQueryVariables
+  >(GetAppParticipantsByAppIdDocument, options);
+}
+export type GetAppParticipantsByAppIdQueryHookResult = ReturnType<
+  typeof useGetAppParticipantsByAppIdQuery
+>;
+export type GetAppParticipantsByAppIdLazyQueryHookResult = ReturnType<
+  typeof useGetAppParticipantsByAppIdLazyQuery
+>;
+export type GetAppParticipantsByAppIdSuspenseQueryHookResult = ReturnType<
+  typeof useGetAppParticipantsByAppIdSuspenseQuery
+>;
+export type GetAppParticipantsByAppIdQueryResult = Apollo.QueryResult<
+  GetAppParticipantsByAppIdQuery,
+  GetAppParticipantsByAppIdQueryVariables
+>;
