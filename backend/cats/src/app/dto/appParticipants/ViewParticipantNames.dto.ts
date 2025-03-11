4f28e90b453ac8709ae43325ed8b8c0f9559511c
nupurdixit13 <nupur.dixit@gov.bc.ca>
2025-03-11 14:57:20 -0700

    Add logic to populate roles + few API

---
+++ backend/cats/src/app/dto/appParticipants/ViewParticipantNames.dto.ts
@@ -0,0 +1,16 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class ViewParticipantNamesDto {
+  @Field()
+  id: number;
+
+  @Field()
+  firstName: string;
+
+  @Field()
+  middleName: string;
+
+  @Field()
+  lastName: string;
+}
