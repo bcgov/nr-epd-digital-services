74a7c2abd24474c9f5424c89de7093f00a249cba
midhun-aot <midhun.murali@aot-technologies.com>
2025-03-19 16:24:24 -0700

    Updated vite config file and updated routes

--- frontend/src/app/routes/Routes.tsx
+++ frontend/src/app/routes/Routes.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Dashboard from "../features/dashboard/Dashboard";
-import UsersList from "../features/users/UsersList";
-import AddUserForm from "../features/users/AddUserForm";
 import Landing from "../features/landing/Landing";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import { SignUp } from "../features/signup/signup";
-import { UserProfile } from "../features/users/UserProfile";
 import Map from "../features/Map/Map";
 import { FileUpload } from "../features/bcbox/FileUpload";
 import TaskAssignment from "../features/assignment/TaskAssignment";
@@ -26,18 +23,12 @@ const AppRoutes = () => {
         path="/dashboard"
         element={<ProtectedRoute element={<Dashboard />} />}
       />
-      <Route
-        path="/profile"
-        element={<ProtectedRoute element={<UserProfile />} />}
-      />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/map" element={<Map />} />
       <Route
         path="/fileupload"
         element={<ProtectedRoute element={<FileUpload />} />}
       />
-      <Route path="/users" element={<UsersList />}></Route>
-      <Route path="/users/add" element={<AddUserForm />}></Route>
       <Route path="*" element={<h1>Page not found</h1>}></Route>
     </Routes>
   );
