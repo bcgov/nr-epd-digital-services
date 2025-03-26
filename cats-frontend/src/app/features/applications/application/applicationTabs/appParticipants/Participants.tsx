e23faf91b5cb7a36b90251ef9cbb49e27b9d7184
nupurdixit13 <nupur.dixit@gov.bc.ca>
2025-03-26 15:04:24 -0700

    Refetching data

--- cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/Participants.tsx
+++ cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/Participants.tsx
@@ -14,10 +14,6 @@ export const Participants = () => {
 
   const { participantColumnInternal } = GetConfig();
 
-  const [data, setData] = useState<any>([]);
-
-  const [refreshParticipants, setRefreshParticipants] = useState(false);
-
   const [filterOption, setFilterOption] = useState<AppParticipantFilter>(
     AppParticipantFilter.All,
   );
@@ -33,19 +29,6 @@ export const Participants = () => {
     },
   });
 
-  useEffect(() => {
-    const refetchData = async () => {
-      await refetch();
-      setData(queryData?.getAppParticipantsByAppId?.data);
-    };
-    refetchData();
-  }, [filterOption, queryData, queryLoading, refreshParticipants]);
-
-  //This takes care to refresh the participants after they are added
-  const handleRefreshParticipants = () => {
-    setRefreshParticipants((prev) => !prev);
-  };
-
   const updateFilter = (newFilter: AppParticipantFilter) => {
     setFilterOption(newFilter);
   };
@@ -56,7 +39,7 @@ export const Participants = () => {
         handleTableChange={() => {}}
         internalRow={participantColumnInternal}
         userType={UserType.Internal}
-        appParticsData={data}
+        appParticsData={queryData?.getAppParticipantsByAppId?.data || []}
         viewMode={UserMode.Default}
         handleTableSort={() => {}}
         handleAddParticipant={() => {}}
@@ -65,7 +48,7 @@ export const Participants = () => {
         loading={queryLoading}
         handleFilterChange={updateFilter}
         filter={filterOption}
-        handleRefreshParticipants={handleRefreshParticipants}
+        handleRefreshParticipants={refetch}
       />
     </div>
   );
