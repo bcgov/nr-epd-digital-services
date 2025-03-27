c82511c77b1f0d4af86d3242aa87ab794ea8451b
nupurdixit13 <nupur.dixit@gov.bc.ca>
2025-03-27 14:24:03 -0700

    feat: SRS -783 : Add App Participant feature (#843)
    * Add the FE functionality for add app participant
    
    * Add CSS for the fields in modal + fix existing FE test cases
    
    * Add the logic to amend the form fields
    
    * Add logic to populate roles + few API
    
    * Add more logic to Add person names
    
    * fix search dropdown issue
    
    * correct the search query and logic for person data
    
    * Fix updating form data to be added for new participant
    
    * fix the dropdown selection issue in InputControls
    
    * Fix dropdown value for participantName
    
    * Fix the search issue for Organization
    
    * Add logic for adding the participant in DB
    
    * Fixed some issue related to adding app participant
    
    * Adding check to avoid duplicate entries
    
    * Fixing the case when the result cache could have same results stored for person and org
    
    * Added Validations and fixed save functionality
    
    * Unit tests for Adding AppParticipant
    
    * return data after adding new particpant
    
    * Removed console and hardcoded appId
    
    * remove console and revert changes made in common component
    
    * revert change made to this file
    
    * Add applicationId for cases wherein no app participants exists
    
    * prettier formatting
    
    * Update InputControls.tsx
    
    * refresh participants when added
    
    * refactor DTO class
    
    * Replace axios call with apollo
    
    * Addressing few review comments
    
    * code formatting
    
    * code cleanup and formatting
    
    * Refetching data
    
    * Change the way form needs to be updated
    
    * remove console.logs
    
    * Address comment
    
    * formatted
    
    * address comments
    
    ---------
    
    Co-authored-by: Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>

---
+++ backend/cats/src/app/dto/appParticipants/createAppParticipant.dto.ts
@@ -0,0 +1,24 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsDate, IsString } from 'class-validator';
+import { BaseAppParticipantsDto } from './baseAppParticipants.dto';
+
+@InputType()
+export class CreateAppParticipantDto extends BaseAppParticipantsDto{
+    @Field()
+    applicationId: number;
+  
+    @Field()
+    personId: number;
+  
+    @Field()
+    participantRoleId: number;
+  
+    @Field({nullable: true} )
+    organizationId: number | null;
+
+    @Field({nullable:true})
+    createdBy: string;
+  
+    @Field({nullable: true})
+    createdDateTime: Date;
+}
--- cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/graphql/Participants.generated.tsx
+++ cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/graphql/Participants.generated.tsx
@@ -11,6 +11,32 @@ export type GetAppParticipantsByAppIdQueryVariables = Types.Exact<{
 
 export type GetAppParticipantsByAppIdQuery = { __typename?: 'Query', getAppParticipantsByAppId: { __typename?: 'AppParticipantsResponse', httpStatusCode?: number | null, success?: boolean | null, message?: string | null, timestamp?: string | null, data?: Array<{ __typename?: 'ViewAppParticipantsDto', id: number, applicationId: number, isMainParticipant: boolean, name: string, fullName: string, description: string, effectiveStartDate: any, effectiveEndDate?: any | null, isMinistry: boolean }> | null } };
 
+export type GetParticipantRolesQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type GetParticipantRolesQuery = { __typename?: 'Query', getAllParticipantRoles: { __typename?: 'ParticipantsRolesResponse', httpStatusCode?: number | null, success?: boolean | null, message?: string | null, timestamp?: string | null, data?: Array<{ __typename?: 'ViewParticipantsRolesDto', id: number, description: string }> | null } };
+
+export type GetParticipantNamesQueryVariables = Types.Exact<{
+  searchParam: Types.Scalars['String']['input'];
+}>;
+
+
+export type GetParticipantNamesQuery = { __typename?: 'Query', getParticipantNames: { __typename?: 'DropdownResponse', httpStatusCode?: number | null, success?: boolean | null, message?: string | null, timestamp?: string | null, data?: Array<{ __typename?: 'DropdownDto', key: string, value: string }> | null } };
+
+export type GetOrganizationsQueryVariables = Types.Exact<{
+  searchParamForOrg: Types.Scalars['String']['input'];
+}>;
+
+
+export type GetOrganizationsQuery = { __typename?: 'Query', getOrganizations: { __typename?: 'DropdownResponse', httpStatusCode?: number | null, success?: boolean | null, message?: string | null, timestamp?: string | null, data?: Array<{ __typename?: 'DropdownDto', key: string, value: string }> | null } };
+
+export type CreateAppParticipantMutationVariables = Types.Exact<{
+  newAppParticipant: Types.CreateAppParticipantDto;
+}>;
+
+
+export type CreateAppParticipantMutation = { __typename?: 'Mutation', createAppParticipant: { __typename?: 'CreateAppParticipantsResponse', message?: string | null, httpStatusCode?: number | null, success?: boolean | null, timestamp?: string | null, data?: Array<{ __typename?: 'ViewAppParticipantEntityDto', id: number, applicationId: number, personId: number, participantRoleId: number, organizationId?: number | null, isMainParticipant: boolean, effectiveStartDate: any, effectiveEndDate?: any | null, createdBy: string, createdDateTime: any, rowVersionCount?: number | null, updatedBy?: string | null, updatedDateTime?: any | null }> | null } };
+
 
 export const GetAppParticipantsByAppIdDocument = gql`
     query getAppParticipantsByAppId($applicationId: Int!, $filter: AppParticipantFilter!) {
@@ -67,3 +93,194 @@ export type GetAppParticipantsByAppIdQueryHookResult = ReturnType<typeof useGetA
 export type GetAppParticipantsByAppIdLazyQueryHookResult = ReturnType<typeof useGetAppParticipantsByAppIdLazyQuery>;
 export type GetAppParticipantsByAppIdSuspenseQueryHookResult = ReturnType<typeof useGetAppParticipantsByAppIdSuspenseQuery>;
 export type GetAppParticipantsByAppIdQueryResult = Apollo.QueryResult<GetAppParticipantsByAppIdQuery, GetAppParticipantsByAppIdQueryVariables>;
+export const GetParticipantRolesDocument = gql`
+    query getParticipantRoles {
+  getAllParticipantRoles {
+    httpStatusCode
+    success
+    message
+    timestamp
+    data {
+      id
+      description
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetParticipantRolesQuery__
+ *
+ * To run a query within a React component, call `useGetParticipantRolesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetParticipantRolesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetParticipantRolesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetParticipantRolesQuery(baseOptions?: Apollo.QueryHookOptions<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>(GetParticipantRolesDocument, options);
+      }
+export function useGetParticipantRolesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>(GetParticipantRolesDocument, options);
+        }
+export function useGetParticipantRolesSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>(GetParticipantRolesDocument, options);
+        }
+export type GetParticipantRolesQueryHookResult = ReturnType<typeof useGetParticipantRolesQuery>;
+export type GetParticipantRolesLazyQueryHookResult = ReturnType<typeof useGetParticipantRolesLazyQuery>;
+export type GetParticipantRolesSuspenseQueryHookResult = ReturnType<typeof useGetParticipantRolesSuspenseQuery>;
+export type GetParticipantRolesQueryResult = Apollo.QueryResult<GetParticipantRolesQuery, GetParticipantRolesQueryVariables>;
+export const GetParticipantNamesDocument = gql`
+    query getParticipantNames($searchParam: String!) {
+  getParticipantNames(searchParam: $searchParam) {
+    httpStatusCode
+    success
+    message
+    timestamp
+    data {
+      key
+      value
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetParticipantNamesQuery__
+ *
+ * To run a query within a React component, call `useGetParticipantNamesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetParticipantNamesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetParticipantNamesQuery({
+ *   variables: {
+ *      searchParam: // value for 'searchParam'
+ *   },
+ * });
+ */
+export function useGetParticipantNamesQuery(baseOptions: Apollo.QueryHookOptions<GetParticipantNamesQuery, GetParticipantNamesQueryVariables> & ({ variables: GetParticipantNamesQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetParticipantNamesQuery, GetParticipantNamesQueryVariables>(GetParticipantNamesDocument, options);
+      }
+export function useGetParticipantNamesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetParticipantNamesQuery, GetParticipantNamesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetParticipantNamesQuery, GetParticipantNamesQueryVariables>(GetParticipantNamesDocument, options);
+        }
+export function useGetParticipantNamesSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetParticipantNamesQuery, GetParticipantNamesQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetParticipantNamesQuery, GetParticipantNamesQueryVariables>(GetParticipantNamesDocument, options);
+        }
+export type GetParticipantNamesQueryHookResult = ReturnType<typeof useGetParticipantNamesQuery>;
+export type GetParticipantNamesLazyQueryHookResult = ReturnType<typeof useGetParticipantNamesLazyQuery>;
+export type GetParticipantNamesSuspenseQueryHookResult = ReturnType<typeof useGetParticipantNamesSuspenseQuery>;
+export type GetParticipantNamesQueryResult = Apollo.QueryResult<GetParticipantNamesQuery, GetParticipantNamesQueryVariables>;
+export const GetOrganizationsDocument = gql`
+    query getOrganizations($searchParamForOrg: String!) {
+  getOrganizations(searchParamForOrg: $searchParamForOrg) {
+    httpStatusCode
+    success
+    message
+    timestamp
+    data {
+      key
+      value
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetOrganizationsQuery__
+ *
+ * To run a query within a React component, call `useGetOrganizationsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetOrganizationsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetOrganizationsQuery({
+ *   variables: {
+ *      searchParamForOrg: // value for 'searchParamForOrg'
+ *   },
+ * });
+ */
+export function useGetOrganizationsQuery(baseOptions: Apollo.QueryHookOptions<GetOrganizationsQuery, GetOrganizationsQueryVariables> & ({ variables: GetOrganizationsQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetOrganizationsQuery, GetOrganizationsQueryVariables>(GetOrganizationsDocument, options);
+      }
+export function useGetOrganizationsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetOrganizationsQuery, GetOrganizationsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetOrganizationsQuery, GetOrganizationsQueryVariables>(GetOrganizationsDocument, options);
+        }
+export function useGetOrganizationsSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetOrganizationsQuery, GetOrganizationsQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetOrganizationsQuery, GetOrganizationsQueryVariables>(GetOrganizationsDocument, options);
+        }
+export type GetOrganizationsQueryHookResult = ReturnType<typeof useGetOrganizationsQuery>;
+export type GetOrganizationsLazyQueryHookResult = ReturnType<typeof useGetOrganizationsLazyQuery>;
+export type GetOrganizationsSuspenseQueryHookResult = ReturnType<typeof useGetOrganizationsSuspenseQuery>;
+export type GetOrganizationsQueryResult = Apollo.QueryResult<GetOrganizationsQuery, GetOrganizationsQueryVariables>;
+export const CreateAppParticipantDocument = gql`
+    mutation createAppParticipant($newAppParticipant: CreateAppParticipantDto!) {
+  createAppParticipant(newAppParticipant: $newAppParticipant) {
+    message
+    httpStatusCode
+    success
+    timestamp
+    data {
+      id
+      applicationId
+      personId
+      participantRoleId
+      organizationId
+      isMainParticipant
+      effectiveStartDate
+      effectiveEndDate
+      createdBy
+      createdDateTime
+      rowVersionCount
+      updatedBy
+      updatedDateTime
+    }
+  }
+}
+    `;
+export type CreateAppParticipantMutationFn = Apollo.MutationFunction<CreateAppParticipantMutation, CreateAppParticipantMutationVariables>;
+
+/**
+ * __useCreateAppParticipantMutation__
+ *
+ * To run a mutation, you first call `useCreateAppParticipantMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateAppParticipantMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createAppParticipantMutation, { data, loading, error }] = useCreateAppParticipantMutation({
+ *   variables: {
+ *      newAppParticipant: // value for 'newAppParticipant'
+ *   },
+ * });
+ */
+export function useCreateAppParticipantMutation(baseOptions?: Apollo.MutationHookOptions<CreateAppParticipantMutation, CreateAppParticipantMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateAppParticipantMutation, CreateAppParticipantMutationVariables>(CreateAppParticipantDocument, options);
+      }
+export type CreateAppParticipantMutationHookResult = ReturnType<typeof useCreateAppParticipantMutation>;
+export type CreateAppParticipantMutationResult = Apollo.MutationResult<CreateAppParticipantMutation>;
+export type CreateAppParticipantMutationOptions = Apollo.BaseMutationOptions<CreateAppParticipantMutation, CreateAppParticipantMutationVariables>;
