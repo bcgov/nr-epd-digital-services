9a58ae579e9a4c43bb2cbbd551fe602013919d29
Anton <anton.trublin+bcgov@gmail.com>
2025-04-25 17:48:35 -0700

    remove redundant comments

--- cats-frontend/src/apollo.ts
+++ cats-frontend/src/apollo.ts
@@ -26,13 +26,13 @@ const authLink = setContext((_, { headers }) => {
 
 // Custom link to append operation name to the URI
 const operationNameLink = new ApolloLink((operation, forward) => {
-  // Get the current URI from context or httpLink
   let uri = `${API}${GRAPHQL}`;
   const operationName = operation.operationName;
+
   // Append ?op=operationName or &op=operationName if there are already query params
   const hasQuery = uri.includes('?');
   uri = hasQuery ? `${uri}&op=${operationName}` : `${uri}?op=${operationName}`;
-  // Set the new URI in the context for httpLink to use
+
   operation.setContext(({ headers = {} }) => ({
     uri,
     headers,
