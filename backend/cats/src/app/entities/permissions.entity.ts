f187374b342d011df765f7f589332433ef6fcc05
jaspal-aot <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-05-29 14:04:54 -0700

    feat: SRS-930 Permissions Manage People (#911)
    * implement person permissions
    
    * Add test cases
    
    * add permission which is not exists in db
    
    * useEffect issue fix
    
    * fix minor issue
    
    * formatting
    
    * fix test case issue
    
    * Revert "formatting"
    
    This reverts commit eed456306de03fe57b5a5ba95ca9234b6e1649d4.
    
    * formatting frontend
    
    * fix typo
    
    * fix review comment
    
    * add logger and fix typo
    
    * add enum as per review comment
    
    * fix test case

---
+++ backend/cats/src/app/entities/permissions.entity.ts
@@ -0,0 +1,42 @@
+import { Column, Entity, In, Index, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { PersonPermission } from "./personPermissions.entity";
+import { ParticipantRole } from "./participantRole.entity";
+
+@Index('pk_person_permissions', ['id'], { unique: true })
+@Entity('permissions')
+export class Permissions {
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
+
+  @Column('integer', { name: 'role_id' })
+  roleId: number;
+
+  @Column('character varying', { name: 'description', length: 250 })
+  description: string;
+
+  @Column('character varying', { name: 'created_by', length: 20 })
+  createdBy: string;
+
+  @Column('timestamp without time zone', { name: 'created_datetime' })
+  createdDatetime: Date;
+
+  @Column('character varying', {
+    name: 'updated_by',
+    length: 20,
+    nullable: true,
+  })
+  updatedBy: string;
+
+  @Column('timestamp without time zone', {
+    name: 'updated_datetime',
+    nullable: true,
+  })
+  updatedDatetime: Date;
+  
+  @ManyToOne(() => ParticipantRole, participantRole => participantRole.permissions)
+  @JoinColumn([{ name: 'role_id', referencedColumnName: 'id' }])
+  role: ParticipantRole;
+
+  @OneToMany(() => PersonPermission, personPermission => personPermission.permission)
+  personPermissions: PersonPermission[];
+}
