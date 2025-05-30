6a9b8b178e2340fef6ab901b56cb2b4ed15deb54
midhun-aot <midhun.murali@aot-technologies.com>
2025-05-30 12:54:29 -0700

    changes for updated capacity calculation

---
+++ backend/cats/src/migrations/1748473281251-master-script.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class masterScript1748473281251 implements MigrationInterface {
+  name = 'masterScript1748473281251';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "cats"."application_service_type" DROP COLUMN "assignment_factor"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "cats"."application_service_type" ADD "assignment_factor" numeric(3,5) NOT NULL`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "cats"."application_service_type" DROP COLUMN "assignment_factor"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "cats"."application_service_type" ADD "assignment_factor" integer NOT NULL`,
+    );
+  }
+}
--- backend/cats/src/seed/applicationServiceType.seed.ts
+++ backend/cats/src/seed/applicationServiceType.seed.ts
@@ -1,40 +1,13 @@
 import { EntityManager } from 'typeorm';
 import { ApplicationServiceType } from '../app/entities/applicationServiceType.entity';
 import { ParticipantRole } from '../app/entities/participantRole.entity';
+import { ApplicationServiceTypeAssignmentFactor } from '../app/entities/applicationServiceTypeAssignmentFactor';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const serviceTypeJSON = require('./applicationServiceType.json');
 
 export const ApplicationServiceTypeSeeder = async (manager: EntityManager) => {
   try{
-   for (const item of serviceTypeJSON) {
-        const existing = await manager.findOne(ApplicationServiceType, {
-          where: {
-            serviceName: item.description,
-            serviceType: item.type,
-          },
-        });
-
-        if (!existing) {
-          const serviceType = new ApplicationServiceType();
-          serviceType.serviceName = item.description;
-          serviceType.serviceType = item.type;
-          serviceType.assignmentFactor = item.value;
-          await manager.save(serviceType);
-        }
-      }
-
-      await manager.update(
-        ParticipantRole,
-        { abbrev: 'CSWKR' },
-        { assignmentFactor: 2, roleType: 'STAFF' },
-      );
-      
-      await manager.update(
-        ParticipantRole,
-        { abbrev: 'SDM' },
-        { assignmentFactor: 1, roleType: 'STAFF' },
-      );
 
       const mentorResult = await manager.findOne(ParticipantRole, {
         where: { abbrev: 'MENTOR' },
@@ -57,13 +30,101 @@ export const ApplicationServiceTypeSeeder = async (manager: EntityManager) => {
         participantRole.roleType = 'STAFF';
         await manager.save(participantRole);
       }
-      else
-      {
-        await manager.update(
-          ParticipantRole,
-          { abbrev: 'MENTOR' },
-          { assignmentFactor: 1, roleType: 'STAFF' },
-        );
+
+      const caseWorkerRole = await manager.findOne(ParticipantRole, {
+        where: { abbrev: 'CSWKR' },
+      });
+
+      const sdmRole = await manager.findOne(ParticipantRole, {
+        where: { abbrev: 'SDM' },
+      });
+
+      const mentorRole = await manager.findOne(ParticipantRole, {
+        where: { abbrev: 'MENTOR' },
+      });
+
+      if (caseWorkerRole && sdmRole && mentorRole) {
+        for (const item of serviceTypeJSON) {
+          let serviceTypeItem = await manager.findOne(ApplicationServiceType, {
+            where: {
+              serviceName: item.description,
+              serviceType: item.type,
+            },
+          });
+
+          if (!serviceTypeItem) {
+            const serviceTypeCW = new ApplicationServiceType();
+            serviceTypeCW.serviceName = item.description;
+            serviceTypeCW.serviceType = item.type;
+            await manager.save(serviceTypeCW);
+          }
+
+          serviceTypeItem = await manager.findOne(ApplicationServiceType, {
+            where: {
+              serviceName: item.description,
+              serviceType: item.type,
+            },
+          });
+
+          const sdmRoleServiceTypeItem = await manager.findOne(
+            ApplicationServiceTypeAssignmentFactor,
+            {
+              where: {
+                applicationServiceType: { id: serviceTypeItem.id },
+                role: { id: sdmRole.id },
+              },
+            },
+          );
+
+          const mentorRoleServiceTypeItem = await manager.findOne(
+            ApplicationServiceTypeAssignmentFactor,
+            {
+              where: {
+                applicationServiceType: { id: serviceTypeItem.id },
+                role: { id: mentorRole.id },
+              },
+            },
+          );
+
+          const caseWorkerRoleServiceTypeItem = await manager.findOne(
+            ApplicationServiceTypeAssignmentFactor,
+            {
+              where: {
+                applicationServiceType: { id: serviceTypeItem.id },
+                role: { id: caseWorkerRole.id },
+              },
+            },
+          );
+
+          if (!sdmRoleServiceTypeItem) {
+            const sdmRoleServiceType =
+              new ApplicationServiceTypeAssignmentFactor();
+            sdmRoleServiceType.applicationServiceType = serviceTypeItem;
+            sdmRoleServiceType.role = sdmRole;
+            sdmRoleServiceType.assignmentFactor = item.SDM;
+            await manager.save(sdmRoleServiceType);
+          }
+
+          if (!mentorRoleServiceTypeItem) {
+            const mentorRoleServiceType =
+              new ApplicationServiceTypeAssignmentFactor();
+            mentorRoleServiceType.applicationServiceType = serviceTypeItem;
+            mentorRoleServiceType.role = mentorRole;
+            mentorRoleServiceType.assignmentFactor = item.MNTR;
+            await manager.save(mentorRoleServiceType);
+          }
+
+          if (!caseWorkerRoleServiceTypeItem) {
+            const caseWorkerRoleServiceType =
+              new ApplicationServiceTypeAssignmentFactor();
+            caseWorkerRoleServiceType.applicationServiceType = serviceTypeItem;
+            caseWorkerRoleServiceType.role = caseWorkerRole;
+            caseWorkerRoleServiceType.assignmentFactor = item.CW;
+            await manager.save(caseWorkerRoleServiceType);
+          }
+        }
+      } else {
+        throw new Error('Failed to create roles');
       }
     } catch (error) {
       console.log('ApplicationServiceTypeSeeder', error);
