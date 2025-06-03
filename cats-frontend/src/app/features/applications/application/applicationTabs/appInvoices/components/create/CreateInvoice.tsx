dbeda89df0b2c208ee08087adbfe7e30a8d66d80
Brendon O'Laney <Brendon.O'Laney@gov.bc.ca>
2025-06-03 07:23:41 -0700

    Remove unused import

--- cats-frontend/src/app/features/applications/application/applicationTabs/appInvoices/components/create/CreateInvoice.tsx
+++ cats-frontend/src/app/features/applications/application/applicationTabs/appInvoices/components/create/CreateInvoice.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
 import CreateInvoiceForm from './CreateInvoiceForm';
 
 const CreateInvoice: React.FC = () => {
