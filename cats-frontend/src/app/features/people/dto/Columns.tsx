1613c2c50b84245c671b844aefa2598afeaf6229
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-06-18 19:25:00 -0700

    fix search people table css

--- cats-frontend/src/app/features/people/dto/Columns.tsx
+++ cats-frontend/src/app/features/people/dto/Columns.tsx
@@ -1,8 +1,6 @@
-import { TickIcon } from '../../../components/common/icon';
+import { FillEye, TickIcon } from '../../../components/common/icon';
 import { FormFieldType } from '../../../components/input-controls/IFormField';
-import { TableColumn, ColumnSize } from '../../../components/table/TableColumn';
-import { FillPinMapFill } from '../../../components/common/icon';
-import { ReactNode } from 'react';
+import { TableColumn } from '../../../components/table/TableColumn';
 
 const getPeopleSearchResultsColumns = () => {
   const columns: TableColumn[] = [
@@ -81,145 +79,6 @@ const getPeopleSearchResultsColumns = () => {
       true,
       getIconColumnType('Tax Exempt', 'isTaxExempt', ''),
     ),
-    // new TableColumn(
-    //   5,
-    //   "Common Name",
-    //   true,
-    //   "commonName",
-    //   2,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Common Name", "commonName", "")
-    // ),
-    // new TableColumn(
-    //   8,
-    //   "Latitude",
-    //   true,
-    //   "latdeg",
-    //   2,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Latitude", "latdeg", "")
-    // ),
-    // new TableColumn(
-    //   9,
-    //   "Latitude(D,M,S)",
-    //   true,
-    //   "latDegrees,latMinutes,latSeconds",
-    //   3,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Latitude(D,M,S)", "latDegrees,latMinutes,latSeconds", "")
-    // ),
-    // new TableColumn(
-    //   10,
-    //   "Longitude",
-    //   true,
-    //   "longdeg",
-    //   3,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Longitude", "longdeg", "")
-    // ),
-    // new TableColumn(
-    //   11,
-    //   "Longitude(D,M,S)",
-    //   true,
-    //   "longDegrees,longMinutes,longSeconds",
-    //   3,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType(
-    //     "Longitude(D,M,S)",
-    //     "longDegrees,longMinutes,longSeconds",
-    //     ""
-    //   )
-    // ),
-    // new TableColumn(
-    //   12,
-    //   "Lat/Long Reliability",
-    //   true,
-    //   "latlongReliabilityFlag",
-    //   3,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Lat/Long Reliability", "latlongReliabilityFlag", "")
-    // ),
-    // new TableColumn(
-    //   13,
-    //   "Created By",
-    //   true,
-    //   "whoCreated",
-    //   4,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Created By", "whoCreated", "")
-    // ),
-    // new TableColumn(
-    //   14,
-    //   "Date Created",
-    //   true,
-    //   "whenCreated",
-    //   4,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Date Created", "whenCreated", "")
-    // ),
-    // new TableColumn(
-    //   15,
-    //   "Last Updated",
-    //   true,
-    //   "whenCreated",
-    //   4,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Last Updated", "whenCreated", "")
-    // ),
-    // new TableColumn(
-    //   16,
-    //   "Consultant Submitted",
-    //   true,
-    //   "consultantSubmitted",
-    //   4,
-    //   false,
-    //   true,
-    //   1,
-    //   false,
-    //   getColumnType("Consultant Submitted", "consultantSubmitted", "")
-    // ),
-    // new TableColumn(
-    //   17,
-    //   "View",
-    //   true,
-    //   "id",
-    //   4,
-    //   true,
-    //   true,
-    //   1,
-    //   true,
-    //   getLinkColumnType("Map","id","","people/map/") ,
-    //   "people/map/",
-    //   true
-    // ),
-
     {
       id: 18,
       displayName: 'Actions',
@@ -230,15 +89,20 @@ const getPeopleSearchResultsColumns = () => {
       isDefault: true,
       sortOrder: 1,
       isChecked: true,
-      displayType: getLinkColumnType(
-        'Details',
-        'id',
-        '',
-        '/person/',
-        'Details',
-        'Manage People',
-      ),
-      // linkRedirectionURL: "people/details/",
+      displayType: {
+        type: FormFieldType.Link,
+        label: 'View',
+        graphQLPropertyName: 'id',
+        value: '',
+        customLinkValue: 'View',
+        customIcon: <FillEye />,
+        customInputTextCss: 'custom-search-link',
+        tableMode: true,
+        href: '/person/',
+        stickyCol: true,
+        componentName: 'Manage People',
+      },
+      customHeaderCss: 'custom-search-tbl-header custom-search-tbl-header-sticky',
       dynamicColumn: true,
       stickyCol: true,
     },
@@ -247,31 +111,13 @@ const getPeopleSearchResultsColumns = () => {
   return columns;
 };
 
-const getColumnTypeWithSticky = (
-  label: string,
-  propertyName: string,
-  value: string,
-) => {
-  return {
-    type: FormFieldType.Label,
-    label: label,
-    graphQLPropertyName: propertyName,
-    value: value,
-    customLabelCss: 'custom-lbl-text',
-    customInputTextCss: 'custom-input-text',
-    tableMode: true,
-    stickyCol: true,
-  };
-};
-
 const getColumnType = (label: string, propertyName: string, value: string) => {
   return {
     type: FormFieldType.Label,
     label: label,
     graphQLPropertyName: propertyName,
     value: value,
-    customLabelCss: 'custom-lbl-text',
-    customInputTextCss: 'custom-input-text',
+    customInputTextCss: 'custom-search-input-text',
     tableMode: true,
     stickyCol: false,
   };
@@ -295,28 +141,6 @@ const getIconColumnType = (
   };
 };
 
-const getLinkColumnType = (
-  label: string,
-  propertyName: string,
-  value: string,
-  href: string,
-  customLabel?: string,
-  componentName?: string,
-) => {
-  return {
-    type: FormFieldType.Link,
-    label: label,
-    graphQLPropertyName: propertyName,
-    value: value,
-    customLabelCss: 'link-for-table',
-    customInputTextCss: 'link-for-table',
-    tableMode: true,
-    stickyCol: true,
-    href: href,
-    customLinkValue: customLabel ?? null,
-    componentName: componentName ?? '',
-  };
-};
 
 export { getPeopleSearchResultsColumns };
 
