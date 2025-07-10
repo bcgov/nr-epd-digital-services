6e378ec026a2071979a2474d2014c95faac0fe30
midhun-aot <105463561+midhun-aot@users.noreply.github.com>
2025-07-10 16:14:14 -0700

    fix: Adding a log statement. (#980)
    * check-in  types.ts
    
    * updating a log statement

--- backend/cats/src/app/resolvers/application/application.resolver.ts
+++ backend/cats/src/app/resolvers/application/application.resolver.ts
@@ -14,104 +14,114 @@ import { ViewApplicationStatus } from '../../dto/application/viewApplicationStat
 
 @Resolver()
 export class ApplicationResolver {
-    constructor(
-        private readonly applicationService: ApplicationService,
-        private readonly loggerService: LoggerService,
-        private readonly applicationResponse: GenericResponseProvider<
-            ViewApplication[]
-        >,
-        private readonly applicationStatusResponse: GenericResponseProvider<
-            ViewApplicationStatus[]
-        >,
-    ) { }
+  constructor(
+    private readonly applicationService: ApplicationService,
+    private readonly loggerService: LoggerService,
+    private readonly applicationResponse: GenericResponseProvider<
+      ViewApplication[]
+    >,
+    private readonly applicationStatusResponse: GenericResponseProvider<
+      ViewApplicationStatus[]
+    >,
+  ) {}
 
-    @Unprotected()
-    @Mutation(() => ApplicationResponse, { name: 'createApplication' })
-    @UsePipes(new GenericValidationPipe())
-    async createApplication(
-        @Args('application', { type: () => CreateApplication })
-        application: CreateApplication,
-    ) {
-        try {
-            this.loggerService.log('ApplicationResolver.createApplication() start');
-            const result = await this.applicationService.createApplication(application);
+  @Unprotected()
+  @Mutation(() => ApplicationResponse, { name: 'createApplication' })
+  @UsePipes(new GenericValidationPipe())
+  async createApplication(
+    @Args('application', { type: () => CreateApplication })
+    application: CreateApplication,
+  ) {
+    try {
+      this.loggerService.log(
+        'ApplicationResolver.createApplication() start' +
+          JSON.stringify(application),
+      );
+      const result = await this.applicationService.createApplication(
+        application,
+      );
 
-            if (result) {
-                this.loggerService.log(
-                    'ApplicationResolver.createApplication() RES:201 end',
-                );
-                return this.applicationResponse.createResponse(
-                    'Application created successfully',
-                    HttpStatus.CREATED,
-                    true,
-                    [result],
-                );
-            } else {
-                this.loggerService.log(
-                    'ApplicationResolver.createApplication() RES:400 end',
-                );
-                return this.applicationResponse.createResponse(
-                    'Failed to create application',
-                    HttpStatus.BAD_REQUEST,
-                    false,
-                    null,
-                );
-            }
-
-        } catch (error) {
-            this.loggerService.error(
-                'ApplicationResolver.createApplication() error',
-                error,
-            );
-            return this.applicationResponse.createResponse(
-                'Error creating application',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                false,
-                null,
-            );
-        }
+      if (result) {
+        this.loggerService.log(
+          'ApplicationResolver.createApplication() RES:201 end',
+        );
+        return this.applicationResponse.createResponse(
+          'Application created successfully',
+          HttpStatus.CREATED,
+          true,
+          [result],
+        );
+      } else {
+        this.loggerService.log(
+          'ApplicationResolver.createApplication() RES:400 end',
+        );
+        return this.applicationResponse.createResponse(
+          'Failed to create application',
+          HttpStatus.BAD_REQUEST,
+          false,
+          null,
+        );
+      }
+    } catch (error) {
+      this.loggerService.error(
+        'ApplicationResolver.createApplication() error',
+        error,
+      );
+      return this.applicationResponse.createResponse(
+        'Error creating application',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        false,
+        null,
+      );
     }
+  }
 
-    @Unprotected()
-    @Mutation(() => ApplicationStatusResponse, { name: 'updateFormsflowAppId' })
-    @UsePipes(new GenericValidationPipe())
-    async updateFormsflowAppId(
-        @Args('appStatusInput') appStatusInput: UpdateApplicationStatusDto,
-    ) {
-        try {
-            this.loggerService.log('ApplicationResolver.updateFormsflowAppId() start');
-            const result = await this.applicationService.updateFormsflowAppId(appStatusInput);
+  @Unprotected()
+  @Mutation(() => ApplicationStatusResponse, { name: 'updateFormsflowAppId' })
+  @UsePipes(new GenericValidationPipe())
+  async updateFormsflowAppId(
+    @Args('appStatusInput') appStatusInput: UpdateApplicationStatusDto,
+  ) {
+    try {
+      this.loggerService.log(
+        'ApplicationResolver.updateFormsflowAppId() start',
+      );
+      const result = await this.applicationService.updateFormsflowAppId(
+        appStatusInput,
+      );
 
-            if (result) {
-                this.loggerService.log('ApplicationResolver.updateFormsflowAppId() RES:200 end');
-                return this.applicationStatusResponse.createResponse(
-                    'Application status updated successfully',
-                    HttpStatus.OK,
-                    true,
-                    [result],
-                );
-            }
-            else {
-                this.loggerService.log('ApplicationResolver.updateFormsflowAppId()RES:404 end');
-                return this.applicationStatusResponse.createResponse(
-                    'Note not found for update',
-                    HttpStatus.NOT_FOUND,
-                    false,
-                    null,
-                );
-            }
-
-        } catch (error) {
-            this.loggerService.error(
-                'ApplicationResolver.updateFormsflowAppId() error',
-                error,
-            );
-            return this.applicationStatusResponse.createResponse(
-                'Error updating FormsflowAppId',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                false,
-                null,
-            );
-        }
+      if (result) {
+        this.loggerService.log(
+          'ApplicationResolver.updateFormsflowAppId() RES:200 end',
+        );
+        return this.applicationStatusResponse.createResponse(
+          'Application status updated successfully',
+          HttpStatus.OK,
+          true,
+          [result],
+        );
+      } else {
+        this.loggerService.log(
+          'ApplicationResolver.updateFormsflowAppId()RES:404 end',
+        );
+        return this.applicationStatusResponse.createResponse(
+          'Note not found for update',
+          HttpStatus.NOT_FOUND,
+          false,
+          null,
+        );
+      }
+    } catch (error) {
+      this.loggerService.error(
+        'ApplicationResolver.updateFormsflowAppId() error',
+        error,
+      );
+      return this.applicationStatusResponse.createResponse(
+        'Error updating FormsflowAppId',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        false,
+        null,
+      );
     }
+  }
 }
