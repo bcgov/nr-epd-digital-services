1c7886c8e4af6dd10abce0e325ddb585ab5ca3ea
midhun-aot <midhun.murali@aot-technologies.com>
2025-07-14 07:15:27 -0700

    Updated implementation & test cases

--- backend/cats/src/app/resolvers/site/site.resolver.spec.ts
+++ backend/cats/src/app/resolvers/site/site.resolver.spec.ts
@@ -61,7 +61,7 @@ describe('SiteResolver', () => {
       };
 
       siteService.getSiteById = jest.fn().mockResolvedValue({
-        findSiteBySiteId: {
+        findSiteBySiteIdLoggedInUser: {
           data: mockSiteData,
         },
       });
