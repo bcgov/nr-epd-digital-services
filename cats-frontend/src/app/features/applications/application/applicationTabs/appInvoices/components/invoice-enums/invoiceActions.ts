60fb940968b62ca61b656f37e1e67f60f864b4dc
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-07-22 11:13:38 -0700

    Implement Invoice CRUD opteration of FE and BE

---
+++ cats-frontend/src/app/features/applications/application/applicationTabs/appInvoices/components/invoice-enums/invoiceActions.ts
@@ -0,0 +1,12 @@
+export enum InvoiceActions {
+    EDIT_INVOICE = 'Edit Invoice',
+    DELETE_INVOICE = 'Delete Invoice',
+    DUPLICATE_INVOICE = 'Duplicate Invoice',
+    MARK_AS_SENT = 'Mark as Sent',
+    SEND_INVOICE = 'Send Invoice',
+    PREVIEW_INVOICE_PDF = 'Preview PDF',
+    RECORD_INVOICE_PAYMENT = 'Record Payment',
+    ADD_INVOICE_ITEM = 'Add Item',
+    SAVE_INVOICE = 'Save Invoice',
+    CANCEL_INVOICE = 'Cancel'
+}
