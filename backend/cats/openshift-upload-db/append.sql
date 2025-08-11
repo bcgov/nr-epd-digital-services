ebf977e27f1c909ee17ed2b68c3340d9e5fa5e47
acoard-aot <61285798+acoard-aot@users.noreply.github.com>
2025-08-11 11:52:32 -0700

    Deploy prod data to DEV environment (#1016)
    * Dummy commit to make PR to test prod data import process in CI/CD
    
    * fix MENT issue
    
    * Update WAL storage size in dev/test/prod to 2gb.
    
    * add null and undefine check
    
    * Reorganize append.sql
    
    * Update append.sql re-add section per Midhun
    
    * Update append.sql
    
    Added Review In Progress status insert statement
    
    ---------
    
    Co-authored-by: Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
    Co-authored-by: midhun-aot <105463561+midhun-aot@users.noreply.github.com>

--- backend/cats/openshift-upload-db/append.sql
+++ backend/cats/openshift-upload-db/append.sql
@@ -11,7 +11,23 @@ INSERT INTO cats.app_type VALUES (8, 'SRCR', 'Site Risk Classification Report', 
 INSERT INTO cats.app_type VALUES (9, 'SOSC', 'Summary of Site Condition', 16777215, 16711808, true, 4, 0, 'sysadmin', '2020-07-24 15:14:00', 'sysadmin', '2020-07-24 15:14:00', '\x000000000006426a');
 
 
+INSERT INTO cats.status_type VALUES (38, 'New', 'New', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (39, 'Accepted', 'Accepted', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (40, 'Resubmit', 'Additional Information Requested', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (41, 'Rejected', 'Rejected', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (42, 'Resubmitted', 'Resubmitted', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (43, 'Returned', 'Returned', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (44, 'Additional Information Requested', 'Additional Information Requested', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (45, 'Queued', 'Queued', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (46, 'Outcome Decision Made - Satisfactory', 'Outcome Decision Made - Satisfactory', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (47, 'Outcome Decision Made - Unsatisfactory', 'Outcome Decision Made - Unsatisfactory', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
+INSERT INTO cats.status_type VALUES (48, 'Review In Progress', 'Review In Progress', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
 
+
+-- Set "STAFF" role for some, matching earlier migration
+UPDATE cats.participant_role
+SET role_type = 'STAFF'
+WHERE abbrev IN ('SDM', 'CSWKR','MENT');
 
 -- Custom script so that it resets the indexes and sequences in our tables.
 -- Necessary, because wtihout it new inserts will go for conflicting ids.
@@ -57,14 +73,3 @@ BEGIN
 END $$;
 
 
-
-INSERT INTO cats.status_type VALUES (38, 'New', 'New', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (39, 'Accepted', 'Accepted', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (40, 'Resubmit', 'Additional Information Requested', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (41, 'Rejected', 'Rejected', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (42, 'Resubmitted', 'Resubmitted', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (43, 'Returned', 'Returned', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (44, 'Additional Information Requested', 'Additional Information Requested', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (45, 'Queued', 'Queued', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (46, 'Outcome Decision Made - Satisfactory', 'Outcome Decision Made - Satisfactory', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
-INSERT INTO cats.status_type VALUES (47, 'Outcome Decision Made - Unsatisfactory', 'Outcome Decision Made - Unsatisfactory', true, 25, 0, 'system', '2025-06-26 14:46:00', 'system', '2025-06-26 14:46:00', '\x0000000000064255');
