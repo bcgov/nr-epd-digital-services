deb656f1d2daecdc2ccf9c90fc375af0f2002240
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-08-13 11:00:31 -0700

    revert change

--- backend/cats/src/app/services/dashboard/dashboard.service.ts
+++ backend/cats/src/app/services/dashboard/dashboard.service.ts
@@ -271,7 +271,7 @@ export class DashboardService {
         `Error in createRecentViewedApplication: ${error.message}`,
         error,
       );
-      return null;
+      throw error;
     }
   }
 }
