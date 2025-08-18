e53f84650d54469cab2155f27035f288da78ef6f
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-08-18 14:09:21 -0700

    fix: Application Id and SiteId needs to be a link.

--- cats-frontend/src/app/features/applications/search/applicationResults/tableColumnConfig.tsx
+++ cats-frontend/src/app/features/applications/search/applicationResults/tableColumnConfig.tsx
@@ -1,4 +1,4 @@
-import { FileLinesIcon } from '../../../../components/common/icon';
+import { ExternalLink, FileLinesIcon } from '../../../../components/common/icon';
 import { FormFieldType } from '../../../../components/input-controls/IFormField';
 import {
   TableColumn,
@@ -8,37 +8,51 @@ import { formatDateUTC } from '../../../../helpers/utility';
 import StaffAssigned from './staffAssigned';
 import './table.css';
 export const applicationResultColumns = (fromMyTasks: boolean = false) => [
-  {
+   {
     id: 1,
     displayName: 'Application ID',
     active: true,
     graphQLPropertyName: 'id',
-    groupId: 1,
-    disabled: false,
-    isDefault: true,
-    sortOrder: 1,
-    isChecked: true,
-    displayType: { type: FormFieldType.Label, label: 'ID' },
-    linkRedirectionURL: undefined,
-    dynamicColumn: false,
+    displayType: {
+      type: FormFieldType.Link,
+      label: 'Application ID',
+      graphQLPropertyName: 'id',
+      value: '',
+      customInputTextCss: 'custom-applications-input-txt',
+      tableMode: true,
+      href: '/applications/',
+      componentName: 'Applications',
+    },
     columnSize: ColumnSize.Default,
-    customHeaderCss: '',
+    dynamicColumn: false,
+    customHeaderCss: 'custom-applications-header',
   },
   {
     id: 2,
     displayName: 'Site ID',
     active: true,
     graphQLPropertyName: 'siteId',
-    groupId: 1,
-    disabled: false,
-    isDefault: true,
-    sortOrder: 2,
-    isChecked: true,
-    displayType: { type: FormFieldType.Label, label: 'Site ID' },
-    linkRedirectionURL: undefined,
+    displayType: { type: FormFieldType.Label },
+    renderCell: (value: any) => {
+      return (
+        <div className="custom-applications-link-wrapper">
+          <a
+            href={`${import.meta.env.VITE_SITE_REGISTRY_URL}/site/details/${value}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={'custom-applications-input-txt'}
+          >
+            {value}
+            <span className="ps-2 custom-applications-external-link">
+              <ExternalLink />
+            </span>
+          </a>
+        </div>
+      );
+    },
+    columnSize: ColumnSize.Small,
     dynamicColumn: false,
-    columnSize: ColumnSize.Default,
-    customHeaderCss: '',
+    customHeaderCss: 'custom-applications-header',
   },
   {
     id: 3,
