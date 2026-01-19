0d3393d157de55ac5a3c0588cd0266477b034e82
midhun-aot <105463561+midhun-aot@users.noreply.github.com>
2026-01-19 11:34:12 -0800

    fix: updated packages and fixed vulnerabilities (#1199)
    * updated packages and fixed vulnerabilities
    
    * updated frontend and map-package
    
    * removing commented section which was used for testing locally
    
    * Updating react-parcel-map to latest version
    
    * removing unused commands
    
    * Updating node image
    
    ---------
    
    Co-authored-by: nikhila-aot <38471389+nikhila-aot@users.noreply.github.com>

--- backend/applications/src/app/entities/externalUser.entity.ts
+++ backend/applications/src/app/entities/externalUser.entity.ts
@@ -2,7 +2,6 @@ import { Directive, ObjectType, Field } from '@nestjs/graphql';
 import { Application } from './application.entity';
 
 @ObjectType()
-@Directive('@extends')
 @Directive('@key(fields: "id")')
 export class ExternalUser {
   @Field()
