93e200412b2a21f671bc4f989282509b621b683e
midhun-aot <midhun.murali@aot-technologies.com>
2026-01-21 15:32:44 -0800

    commented bceid and bcsc logins

--- frontend/src/app/components/login/LoginDropdown.tsx
+++ frontend/src/app/components/login/LoginDropdown.tsx
@@ -1,35 +1,43 @@
-import { Dropdown } from "react-bootstrap"
-import { useAuth } from "react-oidc-context"
-import './LoginDropdown.css'
+import { Dropdown } from "react-bootstrap";
+import { useAuth } from "react-oidc-context";
+import "./LoginDropdown.css";
 
-export const LoginDropdown = (title:string,width_override?:number) =>{
-    const auth = useAuth();
-    
-    return(
-        <Dropdown id="login-selector-dropdown"  >
-            <Dropdown.Toggle id="login-selector-dropdown-toggle" className="login-btn-lh">
-                {title}
-            </Dropdown.Toggle>
-            <Dropdown.Menu id="login-selector-dropdown-menu">
-                <Dropdown.Header>Select Login Method</Dropdown.Header>
-                <Dropdown.Item as="button" id="login-button-bceid" className="dropdown-item"
+export const LoginDropdown = (title: string, width_override?: number) => {
+  const auth = useAuth();
+
+  return (
+    <Dropdown id="login-selector-dropdown">
+      <Dropdown.Toggle
+        id="login-selector-dropdown-toggle"
+        className="login-btn-lh"
+      >
+        {title}
+      </Dropdown.Toggle>
+      <Dropdown.Menu id="login-selector-dropdown-menu">
+        <Dropdown.Header>Select Login Method</Dropdown.Header>
+        {/* <Dropdown.Item as="button" id="login-button-bceid" className="dropdown-item"
                         onClick={() => auth.signinRedirect(
                                 {extraQueryParams:{'kc_idp_hint':'bceid'}})}>
                         Basic/Business BCeID
-                </Dropdown.Item>
-                <Dropdown.Item as="button" id="login-button-bceid" className="dropdown-item"
+                </Dropdown.Item> */}
+        {/* <Dropdown.Item as="button" id="login-button-bceid" className="dropdown-item"
                         onClick={() => auth.signinRedirect(
                                 {extraQueryParams:{'kc_idp_hint':'bcsc'}})}>
                         BC Services Card
-                </Dropdown.Item>
-                <Dropdown.Divider/>
-                <Dropdown.Item as="button" id="login-button-idir" onClick={() => auth.signinRedirect(
-                            {extraQueryParams:{'kc_idp_hint':'idir'}})}>
-                        IDIR
-                </Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-    )
-}
+                </Dropdown.Item> */}
+        <Dropdown.Divider />
+        <Dropdown.Item
+          as="button"
+          id="login-button-idir"
+          onClick={() =>
+            auth.signinRedirect({ extraQueryParams: { kc_idp_hint: "idir" } })
+          }
+        >
+          IDIR
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
-export default LoginDropdown
+export default LoginDropdown;
