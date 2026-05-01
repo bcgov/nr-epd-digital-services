fd9cee6568898cac2b7f8b8c23dc6aa0495d6be5
midhun-aot <midhun.murali@aot-technologies.com>
2026-05-01 09:12:17 -0700

    updating message

--- cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/ParticipantsTable.tsx
+++ cats-frontend/src/app/features/applications/application/applicationTabs/appParticipants/ParticipantsTable.tsx
@@ -426,6 +426,7 @@ const ParticipantTable: React.FC<IParticipantTableProps> = ({
             setDeleteParticipantId(null);
           }}
         >
+          <div>Are you sure you want to remove this participant ?</div>
           <div>
             This action should only be used to correct errors. For valid
             participation history, use an end date.
