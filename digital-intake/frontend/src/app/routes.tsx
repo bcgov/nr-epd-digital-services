7524402ce7e215254463902c339f7c2be338cb8b
midhun-aot <midhun.murali@aot-technologies.com>
2026-06-15 16:12:41 -0700

    updated tsconfig.* files from latest create vite@latest and created new ui & state folder

--- digital-intake/frontend/src/app/routes.tsx
+++ digital-intake/frontend/src/app/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Dashboard from "../features/dashboard";
-import Application from "../features/applications";
+import Dashboard from "../ui/features/dashboard";
+import Application from "../ui/features/applications";
 
 export const router = createBrowserRouter([
   {
--- digital-intake/frontend/src/main.tsx
+++ digital-intake/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { store } from "./app/store";
+import { store } from "./state/store";
 import { router } from "./app/routes";
 import "./index.css";
 
