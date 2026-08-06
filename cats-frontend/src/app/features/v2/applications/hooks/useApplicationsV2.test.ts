56487dcce1773eacf441bab14cbef7ed9ed308ef
Anton <anton.trublin+bcgov@gmail.com>
2026-08-05 21:00:13 -0700

    format

--- cats-frontend/src/app/features/v2/applications/hooks/useApplicationsV2.test.ts
+++ cats-frontend/src/app/features/v2/applications/hooks/useApplicationsV2.test.ts
@@ -222,11 +222,8 @@ describe('useApplicationsV2', () => {
     );
 
     const { rerender } = renderHookWithQueryRouter(
-      ({
-        variables,
-      }: {
-        variables: SearchApplicationsV2QueryVariables;
-      }) => useApplicationsV2(variables),
+      ({ variables }: { variables: SearchApplicationsV2QueryVariables }) =>
+        useApplicationsV2(variables),
       {
         initialEntries: ['/applications-v2'],
         initialProps: {
