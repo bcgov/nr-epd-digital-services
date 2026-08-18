2463d9adb367f16d37e4428826b8105e5d925c06
midhun-aot <105463561+midhun-aot@users.noreply.github.com>
2026-08-18 10:02:56 -0700

    feat: Adding CHEFS components & build files  (#1307)
    * changes for saving and rendering form submissons from CHEFS
    
    * fixed unit test cases
    
    * chefs common components build

---
+++ cats-frontend/common-hosted-form-service/components/src/components/SimpleBCAddress/Component.ts
@@ -0,0 +1,44 @@
+/* tslint:disable */
+import { Constants } from '../Common/Constants';
+import {
+  ShadowDOMAddressComponent,
+  ShadowDOMAddressConfig,
+} from '../Common/ShadowDOMAddressComponent';
+import editForm from './Component.form';
+
+const ID = 'simplebcaddress';
+const DISPLAY = 'Simple BC Address';
+
+export default class Component extends ShadowDOMAddressComponent {
+  protected getConfig(): ShadowDOMAddressConfig {
+    return {
+      type: 'bcgov',
+      provider: 'custom',
+      componentId: ID,
+      displayName: DISPLAY,
+    };
+  }
+
+  static schema(...extend: any[]) {
+    return ShadowDOMAddressComponent.createSchema(
+      {
+        componentId: ID,
+        displayName: DISPLAY,
+      },
+      ...extend
+    );
+  }
+
+  static get builderInfo() {
+    return {
+      title: DISPLAY,
+      group: 'advanced',
+      icon: 'address-book',
+      weight: 90,
+      documentation: Constants.DEFAULT_HELP_LINK,
+      schema: Component.schema(),
+    };
+  }
+
+  public static readonly editForm = editForm;
+}
