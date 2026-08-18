a0dc833fab21a51c4887935337a2b853520b99cd
midhun-aot <105463561+midhun-aot@users.noreply.github.com>
2026-08-18 16:44:15 -0700

    feat: Frontend changes to display CHEFS submission (#1310)
    * changes for saving and rendering form submissons from CHEFS
    
    * fixed unit test cases
    
    * chefs common components build
    
    * frontend changes for form rendering
    
    * changes for build the chefs formio components
    
    * updating docker file
    
    * updating build steps
    
    * updated docker file
    
    * adding .generated file

--- cats-frontend/src/app/features/applications/application/applicationTabs/application/Application.tsx
+++ cats-frontend/src/app/features/applications/application/applicationTabs/application/Application.tsx
@@ -1,25 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {
-  getApplicationForm,
-  getApplicationFormData,
-  getBundleForms,
-  getExecuteRules,
-  getFormDetails,
-} from './FormioEndpoints';
 import { getUser } from '../../../../../helpers/utility';
 import './Application.css';
-import {
-  GetApplicationByIdQuery,
-  useGetApplicationByIdQuery,
-} from './Application.generated';
+import { useGetSubmissionByApplicationIdQuery } from './Application.generated';
 import LoadingOverlay from '../../../../../components/loader/LoadingOverlay';
 import { Form } from '@formio/react';
-import '@formio/js/dist/formio.full.min.css';
-import { set } from 'date-fns';
+import { Formio } from 'formiojs';
+import 'formiojs/dist/formio.full.min.css';
+import BcGovFormioComponents from '../../../../../../../common-hosted-form-service/components/lib';
 
-type ApplicationDetails =
-  GetApplicationByIdQuery['getApplicationDetailsById']['data'];
+Formio.use(BcGovFormioComponents);
 
 type FormJson = {
   title?: string;
@@ -32,11 +22,6 @@ type Submission = {
   state?: string;
 };
 
-interface ApplicationForm {
-  formId: string;
-  formJson: FormJson;
-}
-
 interface ApplicationProps {}
 
 export const Application: React.FC<ApplicationProps> = () => {
@@ -46,197 +31,48 @@ export const Application: React.FC<ApplicationProps> = () => {
   }
 
   const { id } = useParams<{ id: string }>();
-  const [formData, setFormData] = useState<Submission>({
-    data: {},
-  });
-  const [formJson, setFormJson] = useState<FormJson>({
-    components: [],
-  });
-  const [formType, setFormType] = useState<string | null>(null);
-  const [selectedForms, setSelectedForms] = useState<ApplicationForm[]>([]);
-  const [formsToBeFiltered, setFormsToBeFiltered] = useState<ApplicationForm[]>(
-    [],
-  );
-  const [activeStep, setActiveStep] = useState<number>(0);
+  const [formData, setFormData] = useState<Submission>({ data: {} });
+  const [formJson, setFormJson] = useState<FormJson>({ components: [] });
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [rules, setRules] = useState<any[]>([]);
-
-  const evaluateRule = (rule: any, data: any) => {
-    // Normalize operators
-    let expr = rule
-      .replace(/\s+or\s+/gi, ' || ')
-      .replace(/\s+and\s+/gi, ' && ')
-      .replace(/==/g, '===')
-      .replace(/!=/g, '!==');
-
-    // Replace object keys with actual values
-    expr = expr.replace(/\b\w+\b/g, (match: any) => {
-      if (Object.prototype.hasOwnProperty.call(data, match)) {
-        const val = data[match];
-        return typeof val === 'string' ? `"${val}"` : val;
-      }
-      return match;
-    });
-
-    try {
-      // eslint-disable-next-line no-new-func
-      return new Function(`return (${expr});`)();
-    } catch (e) {
-      console.error('Rule evaluation failed:', e);
-      return false;
-    }
-  };
-
-  useEffect(() => {
-    if (rules.length > 0 && formsToBeFiltered.length > 0) {
-      const filteredForms = formsToBeFiltered.filter((form) => {
-        const formRule = rules.find((rule) => rule.formId === form.formId);
-
-        if (formRule) {
-          return formRule.rules.every((rule: any) => {
-            return evaluateRule(rule, formData?.data);
-          });
-        }
-
-        return true;
-      });
-
-      if (filteredForms.length > 0) {
-        setSelectedForms(filteredForms);
-        setFormJson(filteredForms[0]?.formJson);
-      } else {
-        console.log('No forms passed the filter rules');
-      }
-    }
-  }, [rules, formsToBeFiltered, formData?.data]);
 
   const applicationId = parseInt(id ?? '', 10);
-  const {
-    data,
-    loading: appLoading,
-    error: appError,
-  } = useGetApplicationByIdQuery({
-    variables: {
-      applicationId,
-    },
-    fetchPolicy: 'network-only',
-    skip: !applicationId,
-  });
 
-  const application: ApplicationDetails =
-    data?.getApplicationDetailsById.data ?? null;
-  const formId = application?.formId ?? '';
-  const submissionId = application?.submissionId ?? '';
+  const { data: submissionData, loading: submissionLoading } =
+    useGetSubmissionByApplicationIdQuery({
+      variables: { applicationId },
+      fetchPolicy: 'network-only',
+      skip: !applicationId,
+    });
+
+  const submissionFormData =
+    submissionData?.getSubmissionByApplicationId?.data?.formData;
+  const submissionFormSchema =
+    submissionData?.getSubmissionByApplicationId?.data?.formSchema;
 
   useEffect(() => {
-    if (appLoading) return;
+    if (submissionLoading) return;
 
-    if (!formId || !submissionId) {
-      setError('Form details not found.');
-      setIsLoading(false);
-      return;
-    }
-
-    const fetchApplicationData = async () => {
+    if (submissionFormData) {
       try {
-        setError(null);
-        setIsLoading(true);
-        const res = await getFormDetails(formId);
-        if (res) {
-          const { formType, id } = res?.data;
-          setFormType(formType);
+        const parsed = JSON.parse(submissionFormData);
+        setFormData({ data: parsed });
 
-          const formData = await getApplicationFormData(formId, submissionId);
-          setFormData((prev) => ({ ...prev, data: formData?.data?.data }));
-
-          if (formType === 'bundle') {
-            const bundleForms = await getBundleForms(id);
-            const formPromises = bundleForms?.data?.map(async (form: any) => {
-              const formData = await getApplicationForm(form.formId);
-              return {
-                formId: form.formId,
-                formJson: formData?.data,
-              };
-            });
-
-            const forms = await Promise.all(formPromises);
-            getExecuteRules(id, formData?.data).then((response) => {
-              setRules(response?.data ?? []);
-            });
-
-            setFormsToBeFiltered(forms);
-          }
-
-          if (formType !== 'bundle') {
-            const form = await getApplicationForm(formId);
-            setFormJson(form?.data);
-          }
+        if (submissionFormSchema) {
+          const schemaResponse = JSON.parse(submissionFormSchema);
+          setFormJson(schemaResponse.schema);
         }
-      } catch (err) {
-        console.error('Error fetching application data:', err);
-        setError('Failed to load application data.');
-      } finally {
+
         setIsLoading(false);
+        return;
+      } catch (err) {
+        console.error('Error parsing submission form data:', err);
       }
-    };
-
-    fetchApplicationData();
-
-    return () => {
-      setIsLoading(false);
-      setError(null);
-    };
-  }, [appLoading, formId, submissionId, appError]);
-
-  const handleStepClick = (
-    index: number,
-    e?: React.MouseEvent<HTMLDivElement>,
-  ) => {
-    rippleEffect(e);
-    setActiveStep(index);
-    const selectedForm = selectedForms[index];
-    if (selectedForm) {
-      setFormJson(selectedForm?.formJson || {});
     }
-  };
 
-  const handleBackForm = () => {
-    if (activeStep > 0) {
-      setActiveStep(activeStep - 1);
-      handleStepClick(activeStep - 1);
-    }
-  };
-
-  const handleNextForm = () => {
-    if (activeStep < selectedForms.length - 1) {
-      setActiveStep(activeStep + 1);
-      handleStepClick(activeStep + 1);
-    }
-  };
-
-  useEffect(() => {
-    document.getElementById('main')?.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [activeStep]);
-
-  const rippleEffect = (e?: React.MouseEvent<HTMLDivElement>) => {
-    if (!e) return;
-    const stepDiv = e.currentTarget;
-    const oldRipple = stepDiv.querySelector('.ripple-effect');
-
-    if (oldRipple) oldRipple.remove();
-
-    const ripple = document.createElement('span');
-    ripple.className = 'ripple-effect';
-    const rect = stepDiv.getBoundingClientRect();
-    const size = Math.max(rect.width, rect.height);
-    ripple.style.width = ripple.style.height = `${size}px`;
-    const offsetX = e.clientX - rect.left;
-    const offsetY = e.clientY - rect.top;
-    ripple.style.left = `${offsetX - size / 2}px`;
-    ripple.style.top = `${offsetY - size / 2}px`;
-    stepDiv.appendChild(ripple);
-  };
+    setError('Form details not found.');
+    setIsLoading(false);
+  }, [submissionLoading, submissionFormData, submissionFormSchema]);
 
   if (isLoading) {
     return <LoadingOverlay loading={isLoading} />;
@@ -257,89 +93,34 @@ export const Application: React.FC<ApplicationProps> = () => {
 
   return (
     <div className="application-container" id="main">
-      {formType === 'bundle' ? (
-        <div className="py-2">
-          <div className="application-container padding-bottom-3x mb-1">
-            <div className="mb-3">
-              <div className="steps d-flex flex-wrap flex-sm-nowrap justify-content-between pt-2 pb-1">
-                {selectedForms?.map((step, index) => (
-                  <div
-                    key={index}
-                    className={`step pt-4 pb-4 ${index === activeStep ? 'completed' : ''}`}
-                    onClick={(e) => handleStepClick(index, e)}
-                  >
-                    <div className="step-label-wrap">
-                      <div className="step-label"> {index + 1}</div>
-                    </div>
-                    <h4 className="step-title">
-                      {step?.formJson?.title ?? ''}
-                    </h4>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="px-3 pt-5">
-            <div>
-              <h3 className="px-3 py-2 text-truncate fw-bold">
-                {formJson?.title}
-              </h3>
-              <div className="px-3 py-2">
-                {formJson && formData && (
-                  <Form
-                    src={formJson}
-                    submission={formData}
-                    options={{
-                      hide: { submit: true },
-                      noAlerts: false,
-                      readOnly: true,
-                      viewAsHtml: true,
-                    }}
-                  />
-                )}
-              </div>
-              <div className="d-flex align-items-center justify-content-end px-3 py-2">
-                {activeStep > 0 && (
-                  <button
-                    onClick={handleBackForm}
-                    className="btn btn-secondary mr-2"
-                  >
-                    Previous Form
-                  </button>
-                )}
-                {activeStep < selectedForms?.length - 1 && (
-                  <button onClick={handleNextForm} className="btn btn-primary">
-                    Next Form
-                  </button>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <h3
-            className="ml-3 task-head text-truncate fw-bold"
-            style={{ height: '45px' }}
-          >
-            {formJson?.title}
-          </h3>
-          <div>
-            {formJson && formData && (
-              <Form
-                src={formJson}
-                submission={formData}
-                options={{
+      <div>
+        <h3
+          className="ml-3 task-head text-truncate fw-bold"
+          style={{ height: '45px' }}
+        >
+          {formJson?.title || 'Application Submission'}
+        </h3>
+        <div className="px-3 py-2">
+          {formJson?.components?.length > 0 ? (
+            <Form
+              src={formJson as any}
+              submission={formData}
+              options={
+                {
                   hide: { submit: true },
                   noAlerts: false,
                   readOnly: true,
                   viewAsHtml: true,
-                }}
-              />
-            )}
-          </div>
+                } as any
+              }
+            />
+          ) : (
+            <pre className="submission-data">
+              {JSON.stringify(formData?.data, null, 2)}
+            </pre>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
